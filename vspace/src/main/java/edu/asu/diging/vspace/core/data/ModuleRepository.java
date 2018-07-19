58188222f0a1c28ef8284401d8d98924d8c133aa
Julia Damerow <julia.damerow@googlemail.com>
2018-07-19 13:49:20 -0700

    Adding auditing to keep track of changes

--- vspace/src/main/java/edu/asu/diging/vspace/core/data/ModuleRepository.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/data/ModuleRepository.java
@@ -1,11 +1,13 @@
 package edu.asu.diging.vspace.core.data;
 
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.asu.diging.vspace.core.model.impl.Module;
 
 @Repository
+@JaversSpringDataAuditable
 public interface ModuleRepository extends PagingAndSortingRepository<Module, String> {
 
 }
--- vspace/src/main/java/edu/asu/diging/vspace/core/data/SpaceRepository.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/data/SpaceRepository.java
@@ -1,11 +1,13 @@
 package edu.asu.diging.vspace.core.data;
 
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.asu.diging.vspace.core.model.impl.Space;
 
 @Repository
+@JaversSpringDataAuditable
 public interface SpaceRepository extends PagingAndSortingRepository<Space, String> {
 
 }
