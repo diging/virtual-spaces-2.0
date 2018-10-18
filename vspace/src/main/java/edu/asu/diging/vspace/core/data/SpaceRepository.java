07d2e4416f9ba1a65bee43cd0d3b67b4362cb98f
Namratha <Namratha@gbcilsa313grad02imac.dhcp.asu.edu>
2018-10-18 12:32:03 -0700

    [VSPC-12] Formatting imports in Space repository.

--- vspace/src/main/java/edu/asu/diging/vspace/core/data/SpaceRepository.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/data/SpaceRepository.java
@@ -1,9 +1,11 @@
 package edu.asu.diging.vspace.core.data;
 
 import java.util.List;
+
 import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
 import edu.asu.diging.vspace.core.model.impl.Space;
 
 @Repository
