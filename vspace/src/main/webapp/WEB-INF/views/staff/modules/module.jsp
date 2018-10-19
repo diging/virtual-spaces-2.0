4e39b02c522bb48218f728843da591c5cd2c0c35
Neehaarika <neehaarikadasari@gbcilsa313grad03imac.dhcp.asu.edu>
2018-10-19 16:24:48 -0700

    [VSPC-14] TestCases.

--- vspace/src/main/webapp/WEB-INF/views/staff/modules/module.jsp
+++ vspace/src/main/webapp/WEB-INF/views/staff/modules/module.jsp
@@ -1,6 +1,6 @@
 
 	<h1>Module: ${module.name}</h1>
-	<h1>Module: ${module.description}</h1>
+	<h3>Description: ${module.description}</h3>
 	
 <div class="alert alert-light" role="alert">
   Created on <span class="date">${module.creationDate}</span> by ${module.createdBy}.
---
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ModuleManagerTest.java
@@ -0,0 +1,40 @@
+package edu.asu.diging.vspace.core.services.impl;
+
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import edu.asu.diging.vspace.core.data.ModuleRepository;
+import edu.asu.diging.vspace.core.model.IModule;
+import edu.asu.diging.vspace.core.model.impl.Module;
+
+public class ModuleManagerTest {
+	
+	@Mock
+	private ModuleRepository moduleRepo;
+	
+	@InjectMocks
+	private ModuleManager managerToTest = new ModuleManager();
+	
+	@Before
+	  public void init() {
+	    MockitoAnnotations.initMocks(this);
+	  }
+	
+	@Test
+	public void test_getModule_success() {
+		
+		Module newModule = new Module();
+		Optional<Module> findModule = Optional.of(newModule);
+		managerToTest.storeModule(newModule);		
+		Mockito.when(moduleRepo.findById(newModule.getId())).thenReturn(findModule);
+		IModule moduleTest = managerToTest.getModule(findModule.get().getId());
+		Assert.assertEquals(moduleTest, newModule);
+	}
+}
