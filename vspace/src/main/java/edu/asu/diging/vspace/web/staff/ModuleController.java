77377422e98915a923eafba0bec00f676023af08
Neehaarika Dasari <37390861+Neehaarika@users.noreply.github.com>
2019-01-18 16:30:45 -0700

    Story/vspc 14 (#26)
    * [VSPC-14] Modules should have a page.
    
    * [VSPC-14] addressing comments
    
    * [VSPC-14] TestCases.
    
    * [VSPC-14] Formatting
    
    * [VSPC-14] Test cases and related change.
    
    * [VSPC-14] formatted
    
    * [VSPC-14] Test cases refactoring.
    
    * [VSPC-14] Testing ID and title.
    
    * [VSPC-14] Null testcase.
    
    * [VSPC-14] Handling Exception.
    
    * [VSPC-14] idNotExists TestCase Refactoring.
    
    * [VSPC-14] Testcase optimization.
    
    * [VSPC-14] IdNotExists testcase working.
    
    * [VSPC-14] Setting moduleId.

---
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/ModuleController.java
@@ -0,0 +1,24 @@
+package edu.asu.diging.vspace.web.staff;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.asu.diging.vspace.core.model.IModule;
+import edu.asu.diging.vspace.core.services.IModuleManager;
+
+@Controller
+public class ModuleController {
+
+    @Autowired
+    private IModuleManager moduleManager;
+
+    @RequestMapping("/staff/module/{id}")
+    public String showModule(@PathVariable String id, Model model) {
+        model.addAttribute("module", moduleManager.getModule(id));
+
+        return "staff/module";
+    }
+}
---
+++ vspace/src/main/webapp/WEB-INF/views/staff/modules/module.jsp
@@ -0,0 +1,9 @@
+
+	<h1>Module: ${module.name}</h1>
+	<h3>Description: ${module.description}</h3>
+	
+<div class="alert alert-light" role="alert">
+  Created on <span class="date">${module.creationDate}</span> by ${module.createdBy}.
+  <br>
+  Modified on <span class="date">${module.modificationDate}</span> by ${module.modifiedBy}.
+</div>
