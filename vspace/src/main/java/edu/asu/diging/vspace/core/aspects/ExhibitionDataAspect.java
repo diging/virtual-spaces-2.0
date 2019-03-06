ebc5de11b36cb80ff1f764d6e3616f0615de05fb
Julia Damerow <julia.damerow@gmail.com>
2019-03-06 15:42:33 -0700

    [VSPC-26] formatting

--- vspace/src/main/java/edu/asu/diging/vspace/core/aspects/ExhibitionDataAspect.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/aspects/ExhibitionDataAspect.java
@@ -1,6 +1,5 @@
 package edu.asu.diging.vspace.core.aspects;
 
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,30 +15,30 @@ import edu.asu.diging.vspace.core.services.ISpaceManager;
 @Aspect
 public class ExhibitionDataAspect {
 
-	@Autowired
-	private IExhibitionManager exhibitionManager;
-	
-	@Autowired
-	private ISpaceManager spaceManager;
-	
-	@After("execution(public * edu.asu.diging.vspace.web..*Controller.*(..))")
-	public void setExhibition(JoinPoint jp) {
-		Object[] args = jp.getArgs();
-		MethodSignature signature =  (MethodSignature) jp.getSignature();
-		Class<?> returnType = signature.getReturnType();
-		// only if there is a model object injected and if we're returning a string
-		// (assuming that returning a string implies rendering of a view afterwards
-		if (args != null && returnType == String.class) {
-			for (Object obj : args) {
-				if (obj instanceof Model) {
-					if (!((Model)obj).containsAttribute("exhibition")) {
-						((Model)obj).addAttribute("exhibition", exhibitionManager.getStartExhibition());
-					}
-					if (!((Model)obj).containsAttribute("allSpaces")) {
-						((Model)obj).addAttribute("allSpaces", spaceManager.getAllSpaces());
-					}
-				}
-			}
-		}
-	}
+    @Autowired
+    private IExhibitionManager exhibitionManager;
+
+    @Autowired
+    private ISpaceManager spaceManager;
+
+    @After("execution(public * edu.asu.diging.vspace.web..*Controller.*(..))")
+    public void setExhibition(JoinPoint jp) {
+        Object[] args = jp.getArgs();
+        MethodSignature signature = (MethodSignature) jp.getSignature();
+        Class<?> returnType = signature.getReturnType();
+        // only if there is a model object injected and if we're returning a string
+        // (assuming that returning a string implies rendering of a view afterwards
+        if (args != null && returnType == String.class) {
+            for (Object obj : args) {
+                if (obj instanceof Model) {
+                    if (!((Model) obj).containsAttribute("exhibition")) {
+                        ((Model) obj).addAttribute("exhibition", exhibitionManager.getStartExhibition());
+                    }
+                    if (!((Model) obj).containsAttribute("allSpaces")) {
+                        ((Model) obj).addAttribute("allSpaces", spaceManager.getAllSpaces());
+                    }
+                }
+            }
+        }
+    }
 }
