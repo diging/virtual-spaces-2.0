db788980f1ca46b71fb7501524e403b2ea9a28ef
Julia Damerow <julia.damerow@googlemail.com>
2019-04-03 14:29:37 -0700

    [VSPC-33] use simple users for reset password

---
+++ vspace/src/main/java/edu/asu/diging/vspace/web/ResetPasswordRequestController.java
@@ -0,0 +1,14 @@
+package edu.asu.diging.vspace.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ResetPasswordRequestController {
+
+    @RequestMapping(value="/reset/password/request")
+    public String show(Model model) {
+        return "reset/password/request";
+    }
+}
