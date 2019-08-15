b0c54fd723f12bfcf44779fd97928c5922c48cfa
tjquinn1 <tjquinn1@asu.edu>
2019-08-15 15:48:14 -0700

    Removed unused imports in content block manager

--- vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ContentBlockManagerTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ContentBlockManagerTest.java
@@ -9,49 +9,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import edu.asu.diging.vspace.core.data.ImageContentBlockRepository;
-import edu.asu.diging.vspace.core.data.ImageRepository;
-import edu.asu.diging.vspace.core.data.ModuleRepository;
-import edu.asu.diging.vspace.core.data.SlideRepository;
 import edu.asu.diging.vspace.core.data.TextContentBlockRepository;
 import edu.asu.diging.vspace.core.exception.BlockDoesNotExistException;
-import edu.asu.diging.vspace.core.factory.IImageBlockFactory;
-import edu.asu.diging.vspace.core.factory.IImageFactory;
-import edu.asu.diging.vspace.core.factory.ITextBlockFactory;
-import edu.asu.diging.vspace.core.file.IStorageEngine;
 
 public class ContentBlockManagerTest {
-    @Mock
-    private ITextBlockFactory textBlockFactory;
-
-    @Mock
-    private IImageBlockFactory imageBlockFactory;
-
-    @Mock
-    private SlideManager slideManager;
-
-    @Mock
-    private ModuleManager moduleManager;
-
     @Mock
     private TextContentBlockRepository textBlockRepo;
 
     @Mock
     private ImageContentBlockRepository imageBlockRepo;
-
-    @Mock
-    private IImageFactory imageFactory;
-
-    @Mock
-    private ImageRepository imageRepo;
-
-    @Mock
-    private SlideRepository slideRepo;
-
-    @Mock
-    private ModuleRepository moduleRepo;
-
-    @Mock
-    private IStorageEngine storage;
 
     @InjectMocks
     private ContentBlockManager managerToTest;
