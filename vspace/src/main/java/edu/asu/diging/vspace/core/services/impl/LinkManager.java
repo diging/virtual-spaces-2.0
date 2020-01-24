b4dec68d3178f8251c7bdcb2d6b27d7273756b53
Ashmi Chheda <ashmichheda@gmail.com>
2020-01-24 14:01:53 -0700

    Update LinkManager.java

--- vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/LinkManager.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/LinkManager.java
@@ -187,7 +187,6 @@ public class LinkManager implements ILinkManager {
         space.getModuleLinks().remove(link);
         moduleLinkRepo.delete((ModuleLink) link);
     }
- 
     
     @Override
     public void deleteExternalLink(String linkId) {
