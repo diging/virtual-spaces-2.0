685c7f457194b4b2d10d861d76b0f4a5571b5af5
ashmichheda <ashmichheda@tclasol-c02tp1b3j1g6.3500.dhcp.asu.edu>
2020-02-28 11:46:40 -0700

    [VSPC-54] Removed comments and sysouts.

--- vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/SlideManager.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/SlideManager.java
@@ -62,7 +62,6 @@ public class SlideManager implements ISlideManager {
             outer: for (Sequence sequence : sequences) {
                 for (ISlide slideIds : sequence.getSlides()) {
                     if (slideIds.getId().equals(slideId)) {
-                        System.out.println("Slide IDS match -----> ");
                         sequence.getSlides().remove(slideIds);
                         sequenceRepo.save(sequence);
                         continue outer;
@@ -71,29 +70,6 @@ public class SlideManager implements ISlideManager {
             }
             slideRepo.delete((Slide) getSlide(slideId));
 
-//			if (flag.equals("0")) {
-//				slideRepo.delete((Slide) getSlide(slideId));
-//			}
-//			// Slide part of another sequence
-//			else {
-//				List<Sequence> sequences = sequenceRepo.findSequencesForModule(moduleId);
-//				int sizeOfSequences = sequences.size();
-//				for (int i = 0; i < sizeOfSequences; i++) {
-//					
-//					if (sequences.get(i).getSlides().size() > 0) {
-//						int sizeOfSlides = sequences.get(i).getSlides().size();
-//						
-//						for(int j = 0; j < sizeOfSlides; j++) {
-//							if (sequences.get(i).getSlides().get(j).getId().equals(slideId)) {
-//								sequences.get(i).getSlides().remove(j);
-//								sequenceRepo.save(sequences.get(i));
-//								slideRepo.delete((Slide) getSlide(slideId));
-//							}
-//						}
-//						
-//					}
-//				}
-//			}
         } catch (IllegalArgumentException exception) {
             throw new SlideDoesNotExistException(exception);
         }
