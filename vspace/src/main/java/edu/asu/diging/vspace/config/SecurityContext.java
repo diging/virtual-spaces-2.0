bce97e944e9488183e12d96eda1df5440b5e8272
Julia Damerow <julia.damerow@gmail.com>
2020-03-12 10:34:02 -0400

    [VSPC-75] make only setup/admin public

--- vspace/src/main/java/edu/asu/diging/vspace/config/SecurityContext.java
+++ vspace/src/main/java/edu/asu/diging/vspace/config/SecurityContext.java
@@ -42,7 +42,7 @@ public class SecurityContext extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // Anyone can access the urls
                 .antMatchers("/", "/exhibit/**", "/api/**", "/resources/**", "/login",
-                        "/logout", "/register", "/reset/**", "/setup/**").permitAll()
+                        "/logout", "/register", "/reset/**", "/setup/admin").permitAll()
                 // The rest of the our application is protected.
                 .antMatchers("/users/**", "/admin/**", "/staff/user/**").hasRole("ADMIN")
                 .antMatchers("/staff/**").hasAnyRole("STAFF", "ADMIN")
