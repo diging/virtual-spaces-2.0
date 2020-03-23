b2b303b27938101f69d64ae8f4e69888d057aa1b
Sagar-k93 <sagark93@gmail.com>
2020-03-23 10:19:55 -0700

    [VSPC-69] Indentation fix for listimagesController file.

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/ListImagesController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/ListImagesController.java
@@ -15,30 +15,28 @@ import edu.asu.diging.vspace.core.services.IImageService;
 @Controller
 public class ListImagesController {
 
-	@Autowired
-	private IImageService imageService;
+    @Autowired
+    private IImageService imageService;
 
-	@RequestMapping("/staff/images/list/{page}")
-	public String listImagesByPage(@PathVariable String page,
-			@RequestParam(value = "sort", required = false) String sortedBy,
-			@RequestParam(value = "order", required = false) String order, Model model) {
-		int pageNo;
-		try {
-			pageNo = imageService.validatePageNumber(Integer.parseInt(page));
-		} catch (NumberFormatException numberFormatException) {
-			pageNo = 1;
-		}
-		model.addAttribute("totalPages", imageService.getTotalPages());
-		model.addAttribute("currentPageNumber", pageNo);
-		model.addAttribute("totalImageCount", imageService.getTotalImageCount());
-		model.addAttribute("images", imageService.getImages(pageNo, sortedBy, order));
-		model.addAttribute("imageCategories", ImageCategory.values());
-		model.addAttribute("sortProperty",
-				(sortedBy == null || sortedBy.equals("")) ? SortByField.CREATION_DATE.getValue() : sortedBy);
-		model.addAttribute("order",
-				(order == null || order.equals("")) ? Sort.Direction.DESC.toString().toLowerCase() : order);
-
-		return "staff/images/list";
-	}
-
+    @RequestMapping("/staff/images/list/{page}")
+    public String listImagesByPage(@PathVariable String page,
+        @RequestParam(value = "sort", required = false) String sortedBy,
+        @RequestParam(value = "order", required = false) String order, Model model) {
+        int pageNo;
+        try {
+            pageNo = imageService.validatePageNumber(Integer.parseInt(page));
+        } catch (NumberFormatException numberFormatException) {
+            pageNo = 1;
+        }
+        model.addAttribute("totalPages", imageService.getTotalPages());
+        model.addAttribute("currentPageNumber", pageNo);
+        model.addAttribute("totalImageCount", imageService.getTotalImageCount());
+        model.addAttribute("images", imageService.getImages(pageNo, sortedBy, order));
+        model.addAttribute("imageCategories", ImageCategory.values());
+        model.addAttribute("sortProperty",
+            (sortedBy==null || sortedBy.equals("")) ? SortByField.CREATION_DATE.getValue():sortedBy);
+        model.addAttribute("order",
+            (order==null || order.equals("")) ? Sort.Direction.DESC.toString().toLowerCase():order);
+        return "staff/images/list";
+    }
 }
