55c734f41f19120067f416a641d50912cb3c5810
Prashant Ravindra Jadhav <55036442+prjadhav14@users.noreply.github.com>
2020-05-08 16:09:08 -0700

    [VSPC-72]Status check on request to load Space

--- vspace/src/main/java/edu/asu/diging/vspace/web/ExhibitionSpaceController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/ExhibitionSpaceController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.impl.SpaceStatus;
 import edu.asu.diging.vspace.core.services.ILinkManager;
 import edu.asu.diging.vspace.core.services.ISpaceDisplayManager;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
@@ -26,12 +27,17 @@ public class ExhibitionSpaceController {
 	@RequestMapping(value="/exhibit/space/{id}")
 	public String space(@PathVariable("id") String id, Model model) {
 		ISpace space = spaceManager.getSpace(id);
+		if(space.getSpaceStatus().equals(SpaceStatus.Unpublished)) {
+		    model.addAttribute("showAlert", true);
+		    model.addAttribute("message", "Access Denied to the Space");
+		}
+		else {
 		model.addAttribute("space", space);
 		model.addAttribute("spaceLinks", linkManager.getSpaceLinkDisplays(id));
 		model.addAttribute("moduleList", linkManager.getModuleLinkDisplays(id));
 		model.addAttribute("display", spaceDisplayManager.getBySpace(space));
 		model.addAttribute("externalLinkList", linkManager.getExternalLinkDisplays(id));
-		
+		}
 		return "space";
 	}
 }
