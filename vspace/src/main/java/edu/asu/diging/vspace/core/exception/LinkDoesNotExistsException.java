48fd2edad3a3a7f7e94785649bfad1787880059b
Prashant Ravindra Jadhav <55036442+prjadhav14@users.noreply.github.com>
2020-06-17 11:35:31 -0700

    [VSPC-92] Fixing codefactor issues

--- vspace/src/main/java/edu/asu/diging/vspace/core/exception/LinkDoesNotExistsException.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/exception/LinkDoesNotExistsException.java
@@ -5,27 +5,22 @@ public class LinkDoesNotExistsException extends Exception{
 
     public LinkDoesNotExistsException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public LinkDoesNotExistsException(String message, Throwable cause, boolean enableSuppression,
             boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-        // TODO Auto-generated constructor stub
     }
 
     public LinkDoesNotExistsException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 
     public LinkDoesNotExistsException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public LinkDoesNotExistsException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 }
