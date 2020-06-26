fa04057a20b4d4d219fb73071ad4389c383f2458
Sagar Khar <44152998+Sagar-k93@users.noreply.github.com>
2020-06-26 12:50:40 -0600

    Story/vspc 71 (#107)
    * [VSPC-71] Added field 'Mode' on exhibition config page.
    
    * [VSPC-71] Added changes to show custom message on UI if site is in
    different modes.
    
    * [VSPC-71] Changes to remove existing message when user chooses offline
    mode.
    
    * [VSPC-71] Setting up AOP for Exhibition view on mode changes.
    
    * [VSPC-71] Exhibition only shown if user is logged in or exhibition is
    active.
    
    * [VSPC-71] Added modal when user is logged in and sees exhibition if
    exhibition is down.
    
    * [VSPC-71] Changes to show popup only once in exhibition page.
    
    * [VSPC-71] Added new page to redirect public users if exhibition not up.
    
    * [VSPC-71] Formatting changes.
    
    * [VSPC-71] Made changes to address pr comments.
    
    * [VSPC-71] Verified values present before querying them in array.
    
    * [VSPC-71] Created new package for all public view related controllers.
    
    * [VSPC-71] Removed unnecessary code blocks.
    
    * [VSPC-71] Formatting changes.
    
    * [VSPC-71] Changes to make mode enum and modemessage to customMessage.
    
    * [VSPC-71] Removed getAllValues method from ExhibitionModes enum.
    
    * [VSPC-71] Changes to configcontroller to store null if exhibition mode
    is not offline.
    
    * [VSPC-71] Modified customMessage to be retained when mode is changed.
    
    * [VSPC-71] Formatting changes in config.jsp
    
    * [VSPC-71] Change to show correct message on exhibition page pop up for
    maintenance mode.
    
    * [VSPC-71] Changes to handle nullpointer exception when  invalid space is
    accessed.
    
    * [VSPC-71] Formatting changes.
    
    * [VSPC-71] Changes to let aspect around public view controller get by
    passed if not called for space view.
    
    * [VSPC-71] Changed aspect to validate for modules and spaces in
    exhibition.
    
    * [VSPC-71] Removed the comments and prints.
    
    * [VSPC-71] Added constants for prefixes and divided complex method into
    parts.
    
    * [VSPC-71] Divided functionality into multiple methods in aspect for
    exhibition.
    
    * [VSPC-71] Merged with develop and moved exhibition controllers to
    publicview package.
    
    * [VSPC-71] Added changes based on pr review comments.
    
    * [VSPC-71] Indentation changes.
    
    * [VSPC-71] Indentation changes.
    
    * [VSPC-71] Moved ExhibitionSpaceController to publicview package.
    
    * [VSPC-71] Placed the message after pop up back in spaces page.
    
    * [VSPC-71] Made changes to exhibition aspect.
    
    * [VSPC-71] Changes for naming convention and exhibitionModes.
    
    * [VSPC-71] Added comments to aspect methods.
    
    * [VSPC-71] Removed the pop up for the exhibition view.
    
    * [VSPC-71] Added image for exhibition maintenance page.
    
    * [VSPC-71] Made changes as per review comments.
    
    * [VSPC-71] To add Exhibition status in dashboard.
    
    Co-authored-by: skhar <skhar@DESKTOP-94NFI4U>

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/ExhibitionConfigurationController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/ExhibitionConfigurationController.java
@@ -1,9 +1,12 @@
 package edu.asu.diging.vspace.web.staff;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.javers.common.collections.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import edu.asu.diging.vspace.core.data.SpaceRepository;
 import edu.asu.diging.vspace.core.factory.impl.ExhibitionFactory;
+import edu.asu.diging.vspace.core.model.ExhibitionModes;
 import edu.asu.diging.vspace.core.model.IExhibition;
 import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.model.impl.Exhibition;
@@ -40,12 +44,12 @@ public class ExhibitionConfigurationController {
     public String showExhibitions(Model model) {
         // for now we assume there is just one exhibition
         IExhibition exhibition = exhibitManager.getStartExhibition();
-        if (exhibition != null) {
+        if(exhibition!=null) {
             model.addAttribute("exhibition", exhibition);
         } else {
             model.addAttribute("exhibition", new Exhibition());
         }
-        
+        model.addAttribute("exhibitionModes", Arrays.asList(ExhibitionModes.values()));
         model.addAttribute("spacesList", spaceRepo.findAll());
         return "staff/exhibit/config";
     }
@@ -60,26 +64,30 @@ public class ExhibitionConfigurationController {
      */
     @RequestMapping(value = "/staff/exhibit/config", method = RequestMethod.POST)
     public RedirectView createOrUpdateExhibition(HttpServletRequest request,
-            @RequestParam(required = false, name = "exhibitionParam") String exhibitID,
-            @RequestParam("spaceParam") String spaceID, @RequestParam("title") String title, RedirectAttributes attributes)
-            throws IOException {
+        @RequestParam(required = false, name = "exhibitionParam") String exhibitID,
+        @RequestParam("spaceParam") String spaceID, @RequestParam("title") String title,
+        @RequestParam("exhibitMode") ExhibitionModes exhibitMode,
+        @RequestParam(value = "customMessage", required = false, defaultValue = "") String customMessage,
+        RedirectAttributes attributes) throws IOException {
 
         ISpace startSpace = spaceManager.getSpace(spaceID);
 
         Exhibition exhibition;
-        if (exhibitID == null || exhibitID.isEmpty()) {
+        if(exhibitID==null || exhibitID.isEmpty()) {
             exhibition = (Exhibition) exhibitFactory.createExhibition();
         } else {
             exhibition = (Exhibition) exhibitManager.getExhibitionById(exhibitID);
         }
-
         exhibition.setStartSpace(startSpace);
         exhibition.setTitle(title);
+        exhibition.setMode(exhibitMode);
+        if(exhibitMode.equals(ExhibitionModes.OFFLINE) && !customMessage.equals(ExhibitionModes.OFFLINE.getValue())) {
+            exhibition.setCustomMessage(customMessage);
+        }
         exhibition = (Exhibition) exhibitManager.storeExhibition(exhibition);
         attributes.addAttribute("alertType", "success");
         attributes.addAttribute("message", "Successfully Saved!");
         attributes.addAttribute("showAlert", "true");
         return new RedirectView(request.getContextPath() + "/staff/exhibit/config");
     }
-
 }
