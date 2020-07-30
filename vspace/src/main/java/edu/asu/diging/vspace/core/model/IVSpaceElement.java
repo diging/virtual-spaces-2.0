711d9e85d8f4565889c5e262403c904f4e066b50
Julia Damerow <jdamerow@users.noreply.github.com>
2020-07-30 12:25:08 -0400

    Update IVSpaceElement.java

--- vspace/src/main/java/edu/asu/diging/vspace/core/model/IVSpaceElement.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/model/IVSpaceElement.java
@@ -38,8 +38,4 @@ public interface IVSpaceElement {
 
     void setSpaceStatus(SpaceStatus status);
 
-    boolean isShowUnpublishedLinks();
-
-    void setShowUnpublishedLinks(Boolean showUnpublishedLinks);
-
 }
