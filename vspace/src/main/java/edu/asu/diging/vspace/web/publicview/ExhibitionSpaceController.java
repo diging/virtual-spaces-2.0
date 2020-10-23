0d00d503e8939a54a873b11fd3b0d79a45723ef5
ashmichheda <ashmichheda@gmail.com>
2020-10-23 10:56:18 -0700

    [VSPC-89] Added logic to filter space links to hide

--- vspace/src/main/java/edu/asu/diging/vspace/web/publicview/ExhibitionSpaceController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/publicview/ExhibitionSpaceController.java
@@ -1,5 +1,8 @@
 package edu.asu.diging.vspace.web.publicview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import edu.asu.diging.vspace.core.auth.IAuthenticationFacade;
 import edu.asu.diging.vspace.core.model.IExhibition;
 import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.display.ISpaceLinkDisplay;
 import edu.asu.diging.vspace.core.model.impl.SequenceHistory;
 import edu.asu.diging.vspace.core.model.impl.SpaceStatus;
 import edu.asu.diging.vspace.core.services.IExhibitionManager;
@@ -58,9 +62,13 @@ public class ExhibitionSpaceController {
             model.addAttribute("space", space);
             model.addAttribute("moduleList", moduleLinkManager.getLinkDisplays(id));
             if(space.isShowUnpublishedLinks()) {
-                model.addAttribute("spaceLinks",spaceLinkManager.getLinkDisplays(id));
+                List<ISpaceLinkDisplay> spaceLinks = spaceLinkManager.getLinkDisplays(id);
+                List<ISpaceLinkDisplay> filteredSpacelinks = filterLinks(spaceLinks);
+                model.addAttribute("spaceLinks",filteredSpacelinks);
             }else {
-                model.addAttribute("spaceLinks", spaceLinkManager.getSpaceLinkForGivenOrNullSpaceStatus(id, SpaceStatus.PUBLISHED));
+            	List<ISpaceLinkDisplay> spaceLinks = spaceLinkManager.getSpaceLinkForGivenOrNullSpaceStatus(id, SpaceStatus.PUBLISHED);
+                List<ISpaceLinkDisplay> filteredSpacelinks = filterLinks(spaceLinks);
+                model.addAttribute("spaceLinks",filteredSpacelinks);
             }
             model.addAttribute("display", spaceDisplayManager.getBySpace(space));
             model.addAttribute("externalLinkList", externalLinkManager.getLinkDisplays(id));  
@@ -77,4 +85,16 @@ public class ExhibitionSpaceController {
         }
         return "space";
     }
+
+	private List<ISpaceLinkDisplay> filterLinks(List<ISpaceLinkDisplay> spaceLinks) {
+		// Logic to filter the space links with hide links status to true
+		
+		List<ISpaceLinkDisplay> linksToDisplay = new ArrayList<ISpaceLinkDisplay>();
+		for (ISpaceLinkDisplay spaceLinkDisplayObj : spaceLinks) {
+			if(!spaceLinkDisplayObj.getLink().getTargetSpace().isHideLinksToUnpublishedSpaces()) {
+				linksToDisplay.add(spaceLinkDisplayObj);
+			}	
+		}
+		return linksToDisplay;
+	}
 }
