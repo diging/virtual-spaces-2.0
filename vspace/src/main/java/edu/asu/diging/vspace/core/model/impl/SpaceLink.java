b410ed8906795864063dbaa6181df637903154fc
ashmichheda <ashmichheda@gmail.com>
2020-12-17 09:45:20 -0700

    [VSPC-89] Changed line endings and unwanted imports

--- vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/SpaceLink.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/SpaceLink.java
@@ -1,7 +1,5 @@
 package edu.asu.diging.vspace.core.model.impl;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
