16e0b22e4ee8ce4861eabcd1b11a37099de4392d
skhar <sagark93@gmail.com>
2021-02-24 09:14:18 -0700

    [VSPC-115] Added test cases for videoFactory class.

---
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/VideoFactoryTest.java
@@ -0,0 +1,28 @@
+package edu.asu.diging.vspace.core.services.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.asu.diging.vspace.core.factory.impl.VideoFactory;
+import edu.asu.diging.vspace.core.model.IVSVideo;
+
+public class VideoFactoryTest {
+    
+    private VideoFactory videoFactoryToTest = new VideoFactory();
+    
+    @Test
+    public void test_createVideoFile_success() {
+        IVSVideo video =  videoFactoryToTest.createVideo("video1", 1024L, "video/mp4");
+        Assert.assertEquals("video1", video.getFilename());
+        Assert.assertEquals(Long.valueOf(1024L), video.getFileSize());
+        Assert.assertEquals("video/mp4", video.getFileType());
+        Assert.assertEquals(null, video.getUrl());
+    }
+    
+    @Test
+    public void test_createVideoUrl_success() {
+        IVSVideo video = videoFactoryToTest.createVideo("www.testVideo.com/sampleVideo");
+        Assert.assertEquals("www.testVideo.com/sampleVideo", video.getUrl());
+    }
+
+}
