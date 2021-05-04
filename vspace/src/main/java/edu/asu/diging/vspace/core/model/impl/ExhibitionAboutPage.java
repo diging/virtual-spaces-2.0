94241ee41e34789eb7e1d00f6d2b2e75eba12372
Avirup Biswas <avirupbiswas1992@gmail.com>
2021-05-04 16:01:59 -0700

    Changes done in indentation and Java doc

--- vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/ExhibitionAboutPage.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/ExhibitionAboutPage.java
@@ -9,40 +9,45 @@ import javax.persistence.Id;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
+/**
+ * Model for ExhibitionAbout
+ * 
+ * @author abiswa15
+ *
+ */
 @Entity
 public class ExhibitionAboutPage {
-     @Id
-        @GeneratedValue(generator = "exh_abtpg_id_generator")
-        @GenericGenerator(name = "exh_abtpg_id_generator", parameters = @Parameter(name = "prefix", value = "EXHABT"), strategy = "edu.asu.diging.vspace.core.data.IdGenerator")
-        private String id;
+    @Id
+    @GeneratedValue(generator = "exh_abtpg_id_generator")
+    @GenericGenerator(name = "exh_abtpg_id_generator", parameters = @Parameter(name = "prefix", value = "EXHABT"), strategy = "edu.asu.diging.vspace.core.data.IdGenerator")
+    private String id;
 
-        private String title;
-        
-        private String aboutPageText;
+    private String title;
 
-        public String getId() {
-            return id;
-        }
+    private String aboutPageText;
 
-        public void setId(String id) {
-            this.id = id;
-        }
+    public String getId() {
+        return id;
+    }
 
-        public String getTitle() {
-            return title;
-        }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
+    public String getTitle() {
+        return title;
+    }
 
-        public String getAboutPageText() {
-            return aboutPageText;
-        }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-        public void setAboutPageText(String aboutPageText) {
-            this.aboutPageText = aboutPageText;
-        }
+    public String getAboutPageText() {
+        return aboutPageText;
+    }
 
- 
+    public void setAboutPageText(String aboutPageText) {
+        this.aboutPageText = aboutPageText;
+    }
+
 }
