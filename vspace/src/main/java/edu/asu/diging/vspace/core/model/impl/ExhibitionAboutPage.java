64d1b263aef38537a1a1ac68d3898a0f0a9b33ce
AvirupBiswas <39148506+AvirupBiswas@users.noreply.github.com>
2021-05-11 09:53:46 -0700

    Bug/vspc 159 (#210)
    * [VSPC-158] changes made so that vspace logo/name of the exhibition use should get back to first space
    
    * Further changes has been applied for the link the navbar.html so that on Clicking on the vspace logo/name of the exhibition use should get back to first space
    
    * ? has been added in then exhibition?.startSpace?
    
    * changing href link to th:href=@{/}
    
    * New Java Class and html page has been built for AboutPage of Exhibition
    
    * changes done for the About page of Exhibition
    
    * Changes done in indentation and Java doc
    
    * Implemented all the Pull Request Review Comments
    
    * modified one fieldname
    
    * Implemented the changes as mentioned in the Pull Request's comments
    
    * [VSPC-159]  changes made according to review comments mentioned in the pull request
    
    * [VSPC-159] changes done to add Javadoc in IExhibitionAboutPageManager
    
    * [VSPC-159] changes done in the java doc and in controller according to pull request's comments
    
    Co-authored-by: AVIRUP BISWAS (Student) <abiswa15@sundevils.asu.edu>

---
+++ vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/ExhibitionAboutPage.java
@@ -0,0 +1,56 @@
+package edu.asu.diging.vspace.core.model.impl;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * Model for Exhibition About Page
+ * 
+ * @author Avirup Biswas
+ *
+ */
+@Entity
+public class ExhibitionAboutPage {
+    @Id
+    @GeneratedValue(generator = "exh_abtpg_id_generator")
+    @GenericGenerator(name = "exh_abtpg_id_generator", parameters = @Parameter(name = "prefix", value = "EXHABT"), strategy = "edu.asu.diging.vspace.core.data.IdGenerator")
+    private String id;
+    
+    @Lob
+    private String title;
+    
+    @Lob
+    private String aboutPageText;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAboutPageText() {
+        return aboutPageText;
+    }
+
+    public void setAboutPageText(String aboutPageText) {
+        this.aboutPageText = aboutPageText;
+    }
+
+}
---
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/IExhibitionAboutPageManager.java
@@ -0,0 +1,24 @@
+package edu.asu.diging.vspace.core.services;
+
+import java.util.List;
+
+import edu.asu.diging.vspace.core.model.impl.ExhibitionAboutPage;
+/**
+ * IExhibitionAboutPageManager allows to store and manage {@link ExhibitionAboutPage}.
+ * @author Avirup Biswas
+ *
+ */
+public interface IExhibitionAboutPageManager {
+    
+    /**
+     * This method fetches all {@link ExhibitionAboutPage} entries and returns them
+     */
+    List<ExhibitionAboutPage> findAll();
+    
+    /**
+     * This method stores Exhibition About page information and returns the {@link ExhibitionAboutPage} which is being stored
+     * @param {@link ExhibitionAboutPage} This object contains Exhibition about page values to be stored.
+     * @return {@link ExhibitionAboutPage} Returns the object after being stored. Object will now have an ID.
+     */
+    ExhibitionAboutPage store(ExhibitionAboutPage exhibitionAboutPage);
+}
