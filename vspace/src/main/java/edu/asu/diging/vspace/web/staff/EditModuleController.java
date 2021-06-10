e5011ce19d7818cae6c847d9aad4a25296e9db0a
AvirupBiswas <avirupbiswas1992@gmail.com>
2021-06-10 11:13:00 -0700

    [VSPC-168] implemented the story so that name and description of module can be edited

---
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditModuleController.java
@@ -0,0 +1,42 @@
+package edu.asu.diging.vspace.web.staff;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.asu.diging.vspace.core.model.IModule;
+import edu.asu.diging.vspace.core.services.IModuleManager;
+import edu.asu.diging.vspace.web.staff.forms.ModuleForm;
+
+@Controller
+public class EditModuleController {
+
+    @Autowired
+    private IModuleManager moduleManager;
+
+    @RequestMapping(value = "/staff/module/{moduleId}/edit", method = RequestMethod.GET)
+    public String show(Model model, @PathVariable("moduleId") String moduleId) {
+        System.out.println("Heyyyyy");
+        IModule module = moduleManager.getModule(moduleId);
+        ModuleForm moduleForm = new ModuleForm();
+        moduleForm.setName(module.getName());
+        moduleForm.setDescription(module.getDescription());
+        model.addAttribute("moduleForm", moduleForm);
+        model.addAttribute("moduleId", moduleId);
+        return "staff/modules/edit";
+    }
+
+    @RequestMapping(value = "/staff/module/{moduleId}/edit", method = RequestMethod.POST)
+    public String save(@ModelAttribute ModuleForm moduleForm, @PathVariable("moduleId") String moduleId) {
+        IModule module = moduleManager.getModule(moduleId);
+        module.setName(moduleForm.getName());
+        module.setDescription(moduleForm.getDescription());
+        moduleManager.storeModule(module);
+        return "redirect:/staff/module/{moduleId}";
+    }
+
+}
