cd5de7c1eb5af3591c4244bd01f2175b827851af
Pradeep Kumar <pradeepnita2015@gmail.com>
2021-06-14 16:36:52 -0700

    [VSPC-147] Detection of Bibliographies by Zotero - General functionality completed

--- vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/BiblioBlock.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/model/impl/BiblioBlock.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -18,43 +17,32 @@ import edu.asu.diging.vspace.core.model.IBiblioBlock;
 public class BiblioBlock extends ContentBlock implements IBiblioBlock {
 
     private String title;
-    
+
     private String author;
-    
+
     private String year;
-    
+
     private String journal;
-    
+
     private String url;
-    
+
     private String volume;
-    
+
     private String issue;
-    
+
     private String pages;
-    
+
     private String editors;
-    
+
     private String type;
-    
+
     private String note;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see edu.asu.diging.vspace.core.model.impl.IBiblioBlock#getTitle()
-     */
     @Override
     public String getTitle() {
         return title;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * edu.asu.diging.vspace.core.model.impl.IBiblioBlock#setTitle(java.lang.String)
-     */
     @Override
     public void setTitle(String title) {
         this.title = title;
@@ -89,7 +77,7 @@ public class BiblioBlock extends ContentBlock implements IBiblioBlock {
     public void setJournal(String journal) {
         this.journal = journal;
     }
-    
+
     @Override
     public String getUrl() {
         return url;
@@ -163,75 +151,75 @@ public class BiblioBlock extends ContentBlock implements IBiblioBlock {
     @Override
     @Transient
     public String toString() {
-        return "title=" + title + ", author=" + author + ", year=" + year + ", journal=" + journal
-                + ", url=" + url + ", volume=" + volume + ", issue=" + issue + ", pages=" + pages + ", editors="
-                + editors + ", type=" + type + ", note=" + note ;
+        return "title=" + title + ", author=" + author + ", year=" + year + ", journal=" + journal + ", url=" + url
+                + ", volume=" + volume + ", issue=" + issue + ", pages=" + pages + ", editors=" + editors + ", type="
+                + type + ", note=" + note;
     }
-    
+
     @Transient
     public String htmlRenderedBiblio() {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(toString());
         HtmlRenderer renderer = HtmlRenderer.builder().build();
         return renderer.render(document);
-    } 
-    
+    }
+
     @Transient
     public String urlEncodedBiblioMetaData() throws UnsupportedEncodingException {
-        
-        
-        
+
         String urlEncodedBiblioMetaData = "url_ver=Z39.88-2004&ctx_ver=Z39.88-2004";
-        
+
         String ref_id = "info:sid/zotero.org:2";
-        urlEncodedBiblioMetaData += "&rfr_id=" + URLEncoder.encode(ref_id, StandardCharsets.UTF_8.toString()); 
-        
+        urlEncodedBiblioMetaData += "&rfr_id=" + URLEncoder.encode(ref_id, StandardCharsets.UTF_8.toString());
+
         String rft_val_fmt = "info:ofi/fmt:kev:mtx:dissertation";
         urlEncodedBiblioMetaData += "&rft_val_fmt=" + URLEncoder.encode(rft_val_fmt, StandardCharsets.UTF_8.toString());
-        
-        
-//        urlEncodedBiblioMetaData = "url_ver=Z39.88-2004&ctx_ver=Z39.88-2004&rfr_id=info:sid/zotero.org:2&rft_val_fmt=info:ofi/fmt:kev:mtx:dissertation";
-//        System.out.println(StringEscapeUtils.escapeHtml4(urlEncodedBiblioMetaData));
-//        urlEncodedBiblioMetaData = StringEscapeUtils.escapeHtml4(urlEncodedBiblioMetaData);
-        
-        if(title!=null)
-            urlEncodedBiblioMetaData += ("&rft.title=") + URLEncoder.encode(title, StandardCharsets.UTF_8.toString()); 
-        
-        if(author!=null)
-            urlEncodedBiblioMetaData += ("&rft.au=") + URLEncoder.encode(author, StandardCharsets.UTF_8.toString()); 
-        
-        if(year!=null)
+
+        if (type != null)
+            urlEncodedBiblioMetaData += ("&rft.degree=") + URLEncoder.encode(type, StandardCharsets.UTF_8.toString());
+
+        if (title != null)
+            urlEncodedBiblioMetaData += ("&rft.title=") + URLEncoder.encode(title, StandardCharsets.UTF_8.toString());
+
+        if (author != null)
+            urlEncodedBiblioMetaData += ("&rft.au=") + URLEncoder.encode(author, StandardCharsets.UTF_8.toString());
+
+        if (year != null)
             urlEncodedBiblioMetaData += ("&rft.date=") + URLEncoder.encode(year, StandardCharsets.UTF_8.toString());
-        
-        if(journal!=null)
-            urlEncodedBiblioMetaData += ("&rft.jtitle=") + URLEncoder.encode(journal, StandardCharsets.UTF_8.toString());
-        
-        if(url!=null)
+
+        if (journal != null)
+            urlEncodedBiblioMetaData += ("&rft.jtitle=")
+                    + URLEncoder.encode(journal, StandardCharsets.UTF_8.toString());
+
+        if (url != null)
             urlEncodedBiblioMetaData += ("&rft_id=") + URLEncoder.encode(url, StandardCharsets.UTF_8.toString());
-        
-        if(volume!=null)
+
+        if (volume != null)
             urlEncodedBiblioMetaData += ("&rft.volume=") + URLEncoder.encode(volume, StandardCharsets.UTF_8.toString());
-        
-        if(issue!=null)
+
+        if (issue != null)
             urlEncodedBiblioMetaData += ("&rft.issue=") + URLEncoder.encode(issue, StandardCharsets.UTF_8.toString());
-        
-        if(pages!=null)
+
+        if (pages != null) {
             urlEncodedBiblioMetaData += ("&rft.pages=") + URLEncoder.encode(pages, StandardCharsets.UTF_8.toString());
-        
-        if(editors!=null)
-            urlEncodedBiblioMetaData += ("&rft.editors=") + URLEncoder.encode(editors, StandardCharsets.UTF_8.toString());
-        
-        if(type!=null)
-            urlEncodedBiblioMetaData += ("&rft.type=") + URLEncoder.encode(type, StandardCharsets.UTF_8.toString());
-        
-        if(note!=null)
+            String[] tokens = pages.split("-", 2);
+            if (tokens.length > 1) {
+                urlEncodedBiblioMetaData += ("&rft.spage=")
+                        + URLEncoder.encode(tokens[0], StandardCharsets.UTF_8.toString());
+                urlEncodedBiblioMetaData += ("&rft.epage=")
+                        + URLEncoder.encode(tokens[1], StandardCharsets.UTF_8.toString());
+            }
+        }
+
+        if (editors != null)
+            urlEncodedBiblioMetaData += ("&rft.editors=")
+                    + URLEncoder.encode(editors, StandardCharsets.UTF_8.toString());
+
+        if (note != null)
             urlEncodedBiblioMetaData += ("&rft.note=") + URLEncoder.encode(note, StandardCharsets.UTF_8.toString());
-        
+
         urlEncodedBiblioMetaData += ("&rft.language=en");
-        
-//        urlEncodedBiblioMetaData = StringEscapeUtils.escapeHtml4(urlEncodedBiblioMetaData);
-        System.out.println(urlEncodedBiblioMetaData);
-        
+
         return urlEncodedBiblioMetaData;
     }
 
