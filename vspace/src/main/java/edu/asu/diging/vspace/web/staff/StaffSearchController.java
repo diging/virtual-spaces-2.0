0e2236c4767f63d83f7c4b4c9238923c88c63414
AvirupBiswas <avirupbiswas1992@gmail.com>
2021-06-14 16:28:12 -0700

    [VSPC-140] implemented the changes as suggested in PR review comments

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/StaffSearchController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/StaffSearchController.java
@@ -1,10 +1,14 @@
 package edu.asu.diging.vspace.web.staff;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +23,58 @@ public class StaffSearchController {
 
     @Autowired
     private IStaffSearchManager staffSearchManager;
-    
-    @RequestMapping(value="/staff/search")
+
+    @Value("${page_size}")
+    private int pageSize;
+
+    @RequestMapping(value = "/staff/search")
     @GetMapping
-    public String getAllSearchedElements(Model model, @RequestParam(name = "searchText") String searchString) {
-        HashSet<IVSpaceElement> elementList = staffSearchManager.getAllSearchedElements(searchString);
+    public String getAllSearchedElementsWithoutNum(@RequestParam(name = "searchText") String searchString) {
+        return String.format("redirect:/staff/search/1?searchText=%s", searchString);
+    }
+
+    @RequestMapping(value = "/staff/search/{page}")
+    @GetMapping
+    public String getAllSearchedElements(@PathVariable(required = false) String page, Model model,
+            @RequestParam(name = "searchText") String searchString) {
+        HashSet<IVSpaceElement> elementSet = staffSearchManager.getAllSearchedElements(searchString);
+        List<IVSpaceElement> elementList = new ArrayList<>(elementSet);
+        int pageNo;
+        page = StringUtils.isEmpty(page) ? "1" : page;
+        try {
+            pageNo = validatePageNumber(Integer.parseInt(page), elementList.size());
+        } catch (NumberFormatException numberFormatException) {
+            pageNo = 1;
+        }
+        int startIndex = (pageNo - 1) * pageSize;
+        int endIndex = startIndex + pageSize;
+        if (endIndex < elementSet.size()) {
+            elementList = elementList.subList(startIndex, endIndex);
+        } else {
+            if (startIndex < elementSet.size()) {
+                elementList = elementList.subList(startIndex, elementSet.size());
+            }
+        }
+        model.addAttribute("totalPages", getTotalPages(elementSet.size()));
+        model.addAttribute("currentPageNumber", pageNo);
         model.addAttribute("searchWord", searchString);
         model.addAttribute("conIndex", new ExternalLinkValue("0"));
-        model.addAttribute("searchResult", elementList);
+        model.addAttribute("searchResults", elementList);
         model.addAttribute("resultCount", elementList.size());
         return "/staff/search/staffSearch";
     }
-    
+
+    private int validatePageNumber(int pageNo, long totalPages) {
+        if (pageNo < 1) {
+            return 1;
+        } else if (pageNo > totalPages) {
+            return (totalPages == 0) ? 1 : (int) totalPages;
+        }
+        return pageNo;
+    }
+
+    long getTotalPages(long count) {
+        return (count % pageSize == 0) ? count / pageSize : (count / pageSize) + 1;
+    }
+
 }
