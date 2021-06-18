8cfff5a0cc3247a7cee6e70026f3168ffc62a4b2
Pradeep Kumar <pradeepnita2015@gmail.com>
2021-06-18 13:33:42 -0700

    [VSPC-150] Code cleaning for code factor to pass

--- vspace/src/main/java/edu/asu/diging/vspace/core/exception/BibliographyDoesNotExistException.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/exception/BibliographyDoesNotExistException.java
@@ -9,28 +9,23 @@ public class BibliographyDoesNotExistException extends Exception {
 
     public BibliographyDoesNotExistException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public BibliographyDoesNotExistException(String message, Throwable cause, boolean enableSuppression,
             boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-        // TODO Auto-generated constructor stub
     }
 
     public BibliographyDoesNotExistException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 
     public BibliographyDoesNotExistException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public BibliographyDoesNotExistException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
 }
