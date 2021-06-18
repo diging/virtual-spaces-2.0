3f7780441b6311b235ba1c3192abffd52876bf09
AvirupBiswas <avirupbiswas1992@gmail.com>
2021-06-18 12:16:42 -0700

    [VSPC-168] implemented the changes as suggested in the PR review comments

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditModuleController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditModuleController.java
@@ -20,23 +20,36 @@ public class EditModuleController {
 
     @RequestMapping(value = "/staff/module/{moduleId}/edit", method = RequestMethod.GET)
     public String show(Model model, @PathVariable("moduleId") String moduleId) {
-        System.out.println("Heyyyyy");
         IModule module = moduleManager.getModule(moduleId);
+        if(module!=null)
+        {
         ModuleForm moduleForm = new ModuleForm();
         moduleForm.setName(module.getName());
         moduleForm.setDescription(module.getDescription());
         model.addAttribute("moduleForm", moduleForm);
         model.addAttribute("moduleId", moduleId);
         return "staff/modules/edit";
+        }
+        else
+        {
+            return "badrequest";
+        }
     }
 
     @RequestMapping(value = "/staff/module/{moduleId}/edit", method = RequestMethod.POST)
     public String save(@ModelAttribute ModuleForm moduleForm, @PathVariable("moduleId") String moduleId) {
         IModule module = moduleManager.getModule(moduleId);
+        if(module!=null)
+        {
         module.setName(moduleForm.getName());
         module.setDescription(moduleForm.getDescription());
         moduleManager.storeModule(module);
         return "redirect:/staff/module/{moduleId}";
+        }
+        else
+        {
+            return "redirect:/404";
+        }
     }
 
 }
