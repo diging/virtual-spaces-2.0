4695861ad016938dadde992a94b7e03629a2319b
AvirupBiswas <avirupbiswas1992@gmail.com>
2021-06-18 12:19:28 -0700

    [VSPC-168] indentation changed in EditModuleController

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditModuleController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditModuleController.java
@@ -21,17 +21,14 @@ public class EditModuleController {
     @RequestMapping(value = "/staff/module/{moduleId}/edit", method = RequestMethod.GET)
     public String show(Model model, @PathVariable("moduleId") String moduleId) {
         IModule module = moduleManager.getModule(moduleId);
-        if(module!=null)
-        {
-        ModuleForm moduleForm = new ModuleForm();
-        moduleForm.setName(module.getName());
-        moduleForm.setDescription(module.getDescription());
-        model.addAttribute("moduleForm", moduleForm);
-        model.addAttribute("moduleId", moduleId);
-        return "staff/modules/edit";
-        }
-        else
-        {
+        if (module != null) {
+            ModuleForm moduleForm = new ModuleForm();
+            moduleForm.setName(module.getName());
+            moduleForm.setDescription(module.getDescription());
+            model.addAttribute("moduleForm", moduleForm);
+            model.addAttribute("moduleId", moduleId);
+            return "staff/modules/edit";
+        } else {
             return "badrequest";
         }
     }
@@ -39,15 +36,12 @@ public class EditModuleController {
     @RequestMapping(value = "/staff/module/{moduleId}/edit", method = RequestMethod.POST)
     public String save(@ModelAttribute ModuleForm moduleForm, @PathVariable("moduleId") String moduleId) {
         IModule module = moduleManager.getModule(moduleId);
-        if(module!=null)
-        {
-        module.setName(moduleForm.getName());
-        module.setDescription(moduleForm.getDescription());
-        moduleManager.storeModule(module);
-        return "redirect:/staff/module/{moduleId}";
-        }
-        else
-        {
+        if (module != null) {
+            module.setName(moduleForm.getName());
+            module.setDescription(moduleForm.getDescription());
+            moduleManager.storeModule(module);
+            return "redirect:/staff/module/{moduleId}";
+        } else {
             return "redirect:/404";
         }
     }
