521f3cd8b2ef9a0fe56bb9402f10ef2ccc339567
Pradeep Kumar <pradeepnita2015@gmail.com>
2021-08-02 11:41:20 -0700

    Removed complex query for showing public modules only

--- vspace/src/main/java/edu/asu/diging/vspace/core/data/ModuleRepository.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/data/ModuleRepository.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.asu.diging.vspace.core.model.impl.Module;
-import edu.asu.diging.vspace.core.model.impl.Slide;
 
 @Repository
 @JaversSpringDataAuditable
@@ -21,8 +19,5 @@ public interface ModuleRepository extends PagingAndSortingRepository<Module, Str
     public Page<Module> findDistinctByNameContainingOrDescriptionContaining(Pageable requestedPage, String name,
             String description);
     
-    @Query("SELECT DISTINCT m, s.id FROM Module m, ModuleLink lm, Space s WHERE s.spaceStatus = 0 .id = t.id AND m.name LIKE %?1% OR m.description LIKE %?2%")
-    public Page<Slide> findWithNameOrDescription(Pageable requestedPage, String searchText);
-
     List<Module> findAllByOrderByCreationDateDesc();
 }
