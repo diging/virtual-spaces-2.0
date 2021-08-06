d351da1c0b91993744fde182212983533a414ef5
AvirupBiswas <avirupbiswas1992@gmail.com>
2021-08-06 16:38:04 -0700

    [VSPC-140] fixed the compilation error

--- vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/StaffSearchManager.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/StaffSearchManager.java
@@ -1,6 +1,7 @@
 package edu.asu.diging.vspace.core.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
