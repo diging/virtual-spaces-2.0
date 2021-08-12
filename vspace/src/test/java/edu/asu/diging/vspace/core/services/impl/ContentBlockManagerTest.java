2b6d5ca452883c75f0ca068cfcd008019bbc1edb
AvirupBiswas <avirupbiswas1992@gmail.com>
2021-08-12 14:50:33 -0700

    [VSPC-170] updated the unit test cases logic as suggested in the PR review comments

--- vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ContentBlockManagerTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ContentBlockManagerTest.java
@@ -1,8 +1,12 @@
 package edu.asu.diging.vspace.core.services.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,10 +18,8 @@ import org.springframework.dao.EmptyResultDataAccessException;
 
 import edu.asu.diging.vspace.core.data.ContentBlockRepository;
 import edu.asu.diging.vspace.core.data.ImageContentBlockRepository;
-import edu.asu.diging.vspace.core.data.ImageRepository;
 import edu.asu.diging.vspace.core.data.TextContentBlockRepository;
 import edu.asu.diging.vspace.core.exception.BlockDoesNotExistException;
-import edu.asu.diging.vspace.core.model.IVSImage;
 import edu.asu.diging.vspace.core.model.impl.ContentBlock;
 
 public class ContentBlockManagerTest {
@@ -30,66 +32,86 @@ public class ContentBlockManagerTest {
 
     @Mock
     private ImageContentBlockRepository imageBlockRepo;
-
+    
     @InjectMocks
     private ContentBlockManager managerToTest;
 
     private ContentBlock contentBlock;
+    
+    private List<ContentBlock> contentBlockList;
     
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         contentBlock = new ContentBlock();
         contentBlock.setContentOrder(1);
+        
+        ContentBlock contentBlock1 = new ContentBlock();
+        contentBlock1.setContentOrder(2);
+        ContentBlock contentBlock2 = new ContentBlock();
+        contentBlock2.setContentOrder(3);
+        contentBlockList = new ArrayList<>();
+        contentBlockList.add(contentBlock1);
+        contentBlockList.add(contentBlock2);
     }
 
     @Test
     public void test_deleteTextBlockById_success() throws BlockDoesNotExistException {
-        String textBlockId = "2";
-        Optional<ContentBlock> ContentBlockOptional = Optional.of(contentBlock);
-        when(contentBlockRepository.findById("2")).thenReturn(ContentBlockOptional);
-        managerToTest.deleteTextBlockById(textBlockId,null);
+        String textBlockId = "textBlockId_2";
+        Optional<ContentBlock> contentBlockOptional = Optional.of(contentBlock);
+        when(contentBlockRepository.findById("textBlockId_2")).thenReturn(contentBlockOptional);
+        when(contentBlockRepository.findBySlide_IdAndContentOrderGreaterThan("slideId_1",Integer.valueOf(1))).thenReturn(contentBlockList);
+        managerToTest.deleteTextBlockById(textBlockId,"slideId_1");
         Mockito.verify(textBlockRepo).deleteById(textBlockId);
+        List<Integer> contentOrderList = contentBlockList.stream().map(contentBlock -> contentBlock.getContentOrder()).collect(Collectors.toList());
+        assertEquals(Integer.valueOf(1), contentOrderList.get(0));
+        assertEquals(Integer.valueOf(2), contentOrderList.get(1));
+        Mockito.verify(contentBlockRepository).saveAll(contentBlockList);
     }
 
     @Test(expected = BlockDoesNotExistException.class)
     public void test_deleteTextBlockById_forNonExistentId() throws BlockDoesNotExistException {
         String textBlockId = "notARealId";
-        Optional<ContentBlock> ContentBlockOptional = Optional.of(contentBlock);
-        when(contentBlockRepository.findById("notARealId")).thenReturn(ContentBlockOptional);
+        Optional<ContentBlock> contentBlockOptional = Optional.of(contentBlock);
+        when(contentBlockRepository.findById("notARealId")).thenReturn(contentBlockOptional);
         Mockito.doThrow(EmptyResultDataAccessException.class).when(textBlockRepo).deleteById(textBlockId);
-        managerToTest.deleteTextBlockById(textBlockId,null);
+        managerToTest.deleteTextBlockById(textBlockId,"slideId_1");
     }
 
     @Test
     public void test_deleteTextBlockById_whenIdIsNull() throws BlockDoesNotExistException {
         String textBlockId = null;
-        managerToTest.deleteTextBlockById(null,null);
+        managerToTest.deleteTextBlockById(null,"slideId_1");
         Mockito.verify(textBlockRepo, Mockito.never()).deleteById(textBlockId);
     }
 
     @Test
     public void test_deleteImageBlockById_success() throws BlockDoesNotExistException {
-        String imageBlockId = "2";
-        Optional<ContentBlock> ContentBlockOptional = Optional.of(contentBlock);
-        when(contentBlockRepository.findById("2")).thenReturn(ContentBlockOptional);
-        managerToTest.deleteImageBlockById(imageBlockId,null);
+        String imageBlockId = "imageBlockId_2";
+        Optional<ContentBlock> contentBlockOptional = Optional.of(contentBlock);
+        when(contentBlockRepository.findById("imageBlockId_2")).thenReturn(contentBlockOptional);
+        when(contentBlockRepository.findBySlide_IdAndContentOrderGreaterThan("slideId_1",Integer.valueOf(1))).thenReturn(contentBlockList);
+        managerToTest.deleteImageBlockById(imageBlockId,"slideId_1");
         Mockito.verify(imageBlockRepo).deleteById(imageBlockId);
+        List<Integer> contentOrderList = contentBlockList.stream().map(contentBlock -> contentBlock.getContentOrder()).collect(Collectors.toList());
+        assertEquals(Integer.valueOf(1), contentOrderList.get(0));
+        assertEquals(Integer.valueOf(2), contentOrderList.get(1));
+        Mockito.verify(contentBlockRepository).saveAll(contentBlockList);
     }
 
     @Test(expected = BlockDoesNotExistException.class)
     public void test_deleteImageBlockById_forNonExistentId() throws BlockDoesNotExistException {
         String imageBlockId = "notARealId";
-        Optional<ContentBlock> ContentBlockOptional = Optional.of(contentBlock);
-        when(contentBlockRepository.findById("notARealId")).thenReturn(ContentBlockOptional);
+        Optional<ContentBlock> contentBlockOptional = Optional.of(contentBlock);
+        when(contentBlockRepository.findById("notARealId")).thenReturn(contentBlockOptional);
         Mockito.doThrow(EmptyResultDataAccessException.class).when(imageBlockRepo).deleteById(imageBlockId);
-        managerToTest.deleteImageBlockById(imageBlockId,null);
+        managerToTest.deleteImageBlockById(imageBlockId,"slideId_1");
     }
 
     @Test
     public void test_deleteImagetBlockById_whenIdIsNull() throws BlockDoesNotExistException {
         String imageBlockId = null;
-        managerToTest.deleteImageBlockById(null,null);
+        managerToTest.deleteImageBlockById(null,"slideId_1");
         Mockito.verify(imageBlockRepo, Mockito.never()).deleteById(imageBlockId);
 
     }
