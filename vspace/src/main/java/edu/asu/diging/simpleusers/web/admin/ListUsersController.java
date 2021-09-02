debecdc155690036372e45ec0d36c3fbec1058bd
glend17 <glendsouza1702@gmail.com>
2021-09-02 16:26:28 -0700

    [VSPC-186] Added static method to check roles

--- vspace/src/main/java/edu/asu/diging/simpleusers/web/admin/ListUsersController.java
+++ vspace/src/main/java/edu/asu/diging/simpleusers/web/admin/ListUsersController.java
@@ -1,9 +1,14 @@
 package edu.asu.diging.simpleusers.web.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
@@ -33,5 +38,13 @@ public class ListUsersController extends AbstractController {
         model.addObject("exhibition",exhibitionManager.getStartExhibition());
         model.setViewName(configProvider.getUserListView());
         return model;
+    }
+    
+    public static Boolean getRoleAsListOfStrings(Set<SimpleGrantedAuthority> userRoles, String role) {
+        List<String> roleAsString = new ArrayList<String>();
+        for (SimpleGrantedAuthority roleKey : userRoles) {
+            if(roleKey.toString().equals(role)) return true;  
+        }
+        return false;
     }
 }
