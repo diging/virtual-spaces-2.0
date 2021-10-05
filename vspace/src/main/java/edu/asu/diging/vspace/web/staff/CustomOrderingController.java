cead54fda0017583851d7143ea997c3d93df8ef8
glend17 <glendsouza1702@gmail.com>
2021-10-05 16:29:48 -0700

    [VSPC-190] Changed the logic for custom order to input values given by user instead of drag and drop

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/CustomOrderingController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/CustomOrderingController.java
@@ -1,13 +1,38 @@
 package edu.asu.diging.vspace.web.staff;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.asu.diging.vspace.core.data.SpaceRepository;
+import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.impl.Space;
+import edu.asu.diging.vspace.core.model.impl.SpaceStatus;
+import edu.asu.diging.vspace.core.services.ISpaceManager;
 
 @Controller
 public class CustomOrderingController {
- 
+    
+    @Autowired
+    private ISpaceManager spaceManager;
+    
     @RequestMapping("/staff/spaceordering")
-    public String displayCurrentOrder() {
+    public String displayCurrentOrder(Model model) {
+        List<ISpace> spaces = (List<ISpace>) spaceManager.getSpacesWithStatus(SpaceStatus.PUBLISHED);
+        spaces.addAll(spaceManager.getSpacesWithStatus(null));
+        model.addAttribute("spaces", spaces);
+        
+        return "/staff/spaces/customordering";
+    }
+    @RequestMapping("/staff/spaceordering/changeorder")
+    public String changeDisplayOrder(Model model) {
+        List<ISpace> spaces = (List<ISpace>) spaceManager.getSpacesWithStatus(SpaceStatus.PUBLISHED);
+        spaces.addAll(spaceManager.getSpacesWithStatus(null));
+        model.addAttribute("spaces", spaces);
+        
         return "/staff/spaces/customordering";
     }
 
