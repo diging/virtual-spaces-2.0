1ac9e32a0a27b8b8dfa6228838566d0c395c32b6
glend17 <glendsouza1702@gmail.com>
2021-10-07 16:32:18 -0700

    [VSPC-190] Added controller to adjust content order and front end changes

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/SpacesCustomOrderingController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/SpacesCustomOrderingController.java
@@ -2,22 +2,35 @@ package edu.asu.diging.vspace.web.staff;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.asu.diging.vspace.core.data.SpaceRepository;
 import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.SpacesCustomOrder;
 import edu.asu.diging.vspace.core.model.impl.Space;
 import edu.asu.diging.vspace.core.model.impl.SpaceStatus;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
+import edu.asu.diging.vspace.core.services.ISpacesCustomOrderManager;
 
 @Controller
 public class SpacesCustomOrderingController {
     
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    
     @Autowired
     private ISpaceManager spaceManager;
+    
+    @Autowired
+    private ISpacesCustomOrderManager spacesCustomOrderManager;
     
     @RequestMapping("/staff/spaceordering")
     public String displayCurrentOrder(Model model) {
@@ -34,6 +47,17 @@ public class SpacesCustomOrderingController {
         model.addAttribute("spaces", spaces);
         
         return "/staff/spaces/customordering";
+    }
+    
+    @RequestMapping(value = "/staff/spaceordering/update", method = RequestMethod.POST)
+    public ResponseEntity<List<SpacesCustomOrder>> adjustContentOrder(@RequestBody List<SpacesCustomOrder> spacesCustomOrderList) {
+        //try {
+            spacesCustomOrderManager.updateCustomOrder(spacesCustomOrderList);
+//        } catch (SpacesCustomOrderDoesNotExistException e) {
+//            logger.warn("Spaces Custom Order Id does not exist, bad request.", e);
+//            return new ResponseEntity<List<SpacesCustomOrder>>(HttpStatus.BAD_REQUEST);
+//        }
+        return new ResponseEntity<List<SpacesCustomOrder>>(spacesCustomOrderList,HttpStatus.OK);
     }
 
 }
