afb962ca410d0b5db11847d6872e814c3096446a
glend17 <glendsouza1702@gmail.com>
2021-11-03 13:12:18 -0700

    [VSPC-171] Bug checks

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/AddSpaceBlockController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/AddSpaceBlockController.java
@@ -8,15 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.model.ISpaceBlock;
 import edu.asu.diging.vspace.core.services.IContentBlockManager;
 import edu.asu.diging.vspace.core.services.IModuleLinkManager;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
+import edu.asu.diging.vspace.web.staff.forms.SpaceContentBlockForm;
 
 @Controller
 public class AddSpaceBlockController {
@@ -31,15 +34,17 @@ public class AddSpaceBlockController {
     
     @Autowired
     private IModuleLinkManager moduleLinkManager;
-    @RequestMapping(value = "/staff/module/{moduleId}/slide/{id}/{spaceId}/SpaceBlockContent", method = RequestMethod.POST)
-    ResponseEntity<String> addSpaceBlock(@PathVariable("id") String slideId,
-            @PathVariable("moduleId") String moduleId, @PathVariable("SpaceId") String spaceId){
-        
-        ISpace space = spaceManager.getSpace(spaceId);
-        Integer contentOrder = contentBlockManager.findMaxContentOrder(slideId);
-        contentOrder = contentOrder == null ? 0 : contentOrder + 1;
-        ISpaceBlock spaceBlock = contentBlockManager.createSpaceBlock(slideId, moduleId, contentOrder, space);
-        return new ResponseEntity<>(spaceBlock.getTitle(), HttpStatus.OK);
+    @RequestMapping(value = "/staff/module/{moduleId}/slide/{slideId}/SpaceBlockContent", method = RequestMethod.POST)
+    ResponseEntity<String> addSpaceBlock(@ModelAttribute SpaceContentBlockForm spaceUploadForm, @PathVariable("slideId") String slideId,
+            @PathVariable("moduleId") String moduleId){
+        logger.info("inside space block controller selected space {} {}", spaceUploadForm,spaceUploadForm);
+//        String spaceId = spaceForm.getSpaceName();
+//        ISpace space = spaceManager.getSpace(spaceId);
+//        Integer contentOrder = contentBlockManager.findMaxContentOrder(slideId);
+//        contentOrder = contentOrder == null ? 0 : contentOrder + 1;
+//        ISpaceBlock spaceBlock = contentBlockManager.createSpaceBlock(slideId, moduleId, contentOrder, space);
+//        return new ResponseEntity<>(spaceBlock.getTitle(), HttpStatus.OK);
+        return  new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
---
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/forms/SpaceContentBlockForm.java
@@ -0,0 +1,20 @@
+package edu.asu.diging.vspace.web.staff.forms;
+
+public class SpaceContentBlockForm {
+    
+    String title;
+    String spaceName;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getSpaceName() {
+        return spaceName;
+    }
+    public void setSpaceName(String spaceName) {
+        this.spaceName = spaceName;
+    }
+}
