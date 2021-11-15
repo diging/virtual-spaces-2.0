5eb2f6eec1e6ced4163fbd3d72eed1cffb0cf458
glend17 <glendsouza1702@gmail.com>
2021-11-15 09:55:48 -0700

    [VSPC-171] Code factor changes

--- vspace/src/main/java/edu/asu/diging/vspace/core/factory/impl/SpaceBlockFactory.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/factory/impl/SpaceBlockFactory.java
@@ -1,7 +1,5 @@
 package edu.asu.diging.vspace.core.factory.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import edu.asu.diging.vspace.core.factory.ISpaceBlockFactory;
@@ -13,15 +11,12 @@ import edu.asu.diging.vspace.core.model.impl.SpaceBlock;
 @Service
 public class SpaceBlockFactory implements ISpaceBlockFactory {
     
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-    
     @Override
     public ISpaceBlock createSpaceBlock(ISlide slide, String title, ISpace space) {
         ISpaceBlock spaceBlock = new SpaceBlock();
         spaceBlock.setTitle(title);
         spaceBlock.setSlide(slide);
         spaceBlock.setSpace(space);
-        logger.info("space block is created with values {} {} {}", spaceBlock.getSlide().getContents(), spaceBlock.getSlide().getId(), spaceBlock.getSpace().getId());;
         return spaceBlock;
     }
 }
