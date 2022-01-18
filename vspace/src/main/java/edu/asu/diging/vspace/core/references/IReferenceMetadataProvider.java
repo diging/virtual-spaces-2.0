18e33686f9570f798c8e0b2802e01cd53cf1c683
Pradeep Kumar <pradeepnita2015@gmail.com>
2022-01-18 15:53:08 -0700

    [VSPC-150] Changes for Java doc

--- vspace/src/main/java/edu/asu/diging/vspace/core/references/IReferenceMetadataProvider.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/references/IReferenceMetadataProvider.java
@@ -3,6 +3,10 @@ package edu.asu.diging.vspace.core.references;
 import edu.asu.diging.vspace.core.exception.ReferenceMetadataEncodingException;
 import edu.asu.diging.vspace.core.model.impl.Reference;
 
+/**
+ * This interface is extended by different Metadata Provider classes and implements the methods to create the metadata required for a reference
+ *
+ */
 public interface IReferenceMetadataProvider {
     
     public ReferenceMetadataType getReferenceMetadataType();
