491e7b4fd4e19534dbbc4190113939252cbe85d3
glend17 <glendsouza1702@gmail.com>
2022-01-18 23:41:51 -0700

    [VSPC-99] removing redundant null check

--- vspace/src/main/java/edu/asu/diging/vspace/web/publicview/ExhibitionSlideController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/publicview/ExhibitionSlideController.java
@@ -129,10 +129,8 @@ public class ExhibitionSlideController {
         
         ModuleOverview moduleOverview = null;
         moduleOverview = sequenceOverviewManager.showModuleMap(moduleId);
-        if(moduleOverview != null) {
-            moduleOverview.setName(module.getName());
-            moduleOverview.setId(module.getId());
-        }
+        moduleOverview.setName(module.getName());
+        moduleOverview.setId(module.getId());
         model.addAttribute("overview", moduleOverview);
         model.addAttribute("numOfSlides", sequenceSlides.size());
         model.addAttribute("currentNumOfSlide", slideIndex + 1);
