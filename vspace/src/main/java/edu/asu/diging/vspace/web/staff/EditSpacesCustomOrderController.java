4d5ea90ad6590f6962f678ffeb06a8f1834680c9
glend17 <glendsouza1702@gmail.com>
2022-02-03 14:16:50 -0700

    [VSPC-190] enabled drag and drop for space ordering

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditSpacesCustomOrderController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditSpacesCustomOrderController.java
@@ -1,5 +1,9 @@
 package edu.asu.diging.vspace.web.staff;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,8 @@ public class EditSpacesCustomOrderController {
     @Autowired
     private ISpacesCustomOrderManager spacesCustomOrderManager;
     
+    private static Logger logger = LoggerFactory.getLogger(SpacesCustomOrderingController.class);
+    
     @RequestMapping(value = "/staff/spaceordering/{customOrderId}/edit/description", method = RequestMethod.POST)
     public ResponseEntity<String> saveDescription(@RequestParam("description") String description, @PathVariable("customOrderId") String spacesCustomOrderId) {
         SpacesCustomOrder spacesCustomOrder = spacesCustomOrderManager.getSpaceCustomOrderById(spacesCustomOrderId);
@@ -31,6 +37,12 @@ public class EditSpacesCustomOrderController {
         SpacesCustomOrder spacesCustomOrder = spacesCustomOrderManager.getSpaceCustomOrderById(spacesCustomOrderId);
         spacesCustomOrder.setCustomOrderName(name);
         spacesCustomOrderManager.updateSpacesCustomOrder(spacesCustomOrder);
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+    
+    @RequestMapping(value = "/staff/spaceordering/{customOrderId}/edit/spaceorders", method = RequestMethod.POST)
+    public ResponseEntity<String> saveOrder(@RequestParam("spaceOrder") List<String> spaceOrders) {
+        logger.info("space order is {}", spaceOrders.get(0));
         return new ResponseEntity<String>(HttpStatus.OK);
     }
 
