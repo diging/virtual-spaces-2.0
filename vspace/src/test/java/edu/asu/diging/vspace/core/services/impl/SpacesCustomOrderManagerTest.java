6e30a2f4c4f7b387538993de5a8eb0467204fa2b
glend17 <glendsouza1702@gmail.com>
2022-02-10 12:59:09 -0700

    [VSPC-190] adding unit tests

--- vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/SpacesCustomOrderManagerTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/SpacesCustomOrderManagerTest.java
@@ -1,30 +1,24 @@
 package edu.asu.diging.vspace.core.services.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 
 import java.util.Optional;
 
 import edu.asu.diging.vspace.core.data.SpacesCustomOrderRepository;
 import edu.asu.diging.vspace.core.model.IExhibition;
-import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.model.SpacesCustomOrder;
 import edu.asu.diging.vspace.core.model.impl.Exhibition;
 import edu.asu.diging.vspace.core.model.impl.Space;
-import edu.asu.diging.vspace.core.model.impl.SpaceStatus;
 import edu.asu.diging.vspace.core.services.IExhibitionManager;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
 
@@ -42,10 +36,6 @@ public class SpacesCustomOrderManagerTest{
     @InjectMocks
     private SpacesCustomOrderManager serviceToTest;
     
-    @Spy
-    private SpacesCustomOrderManager spyManager;
-    
-    private String spaceId = "spaceId";
     private String spaceCustomOrderId1, spaceCustomOrderId2;
     private IExhibition exhibition = new Exhibition();
     
@@ -59,7 +49,6 @@ public class SpacesCustomOrderManagerTest{
     
     @Test
     public void test_deleteSpaceCustomOrderByIdWithNullSpacesCustomOrder_forSuccess() { 
-        SpacesCustomOrder spacesCustomOrder = null;
         Mockito.when(exhibitionManager.getStartExhibition()).thenReturn(exhibition);
         serviceToTest.deleteSpacesCustomOrderById(spaceCustomOrderId1);
         Mockito.verify(spacesCustomOrderRepository).deleteById(spaceCustomOrderId1);
@@ -76,20 +65,55 @@ public class SpacesCustomOrderManagerTest{
         Mockito.verify(exhibitionManager).storeExhibition((Exhibition)exhibition);
     }
     
-//    @Test
-//    public void test_editSpacesCustomOrders_forSuccess() {
-//        List<String> spaceIds = new ArrayList<String>();
-//        SpacesCustomOrder spacesCustomOrder = new SpacesCustomOrder();
-//        SpacesCustomOrderManager classToTest = null;
-//        SpacesCustomOrderManager spy = Mockito.spy(classToTest);
-//        spaceIds.add("SPC001");
-//        spaceIds.add("SPC002");
-//        Mockito.doReturn(spacesCustomOrder).when(spy).getSpaceCustomOrderById(spaceCustomOrderId1);
-//        serviceToTest.editSpacesCustomOrder(spaceCustomOrderId1, spaceIds);
-//        Mockito.verify(spacesCustomOrderRepository).save(spacesCustomOrder);
-//    }
+    @Test
+    public void test_editSpacesCustomOrders_Success() {
+        String spaceId1 = "SPC001";
+        String spaceId2 = "SPC002";
+        List<String> spaceIdList = new ArrayList<String>();
+        spaceIdList.add(spaceId1);
+        spaceIdList.add(spaceId2);
+        SpacesCustomOrder spacesCustomOrder = new SpacesCustomOrder();
+        when(spacesCustomOrderRepository.findById(spaceCustomOrderId1)).thenReturn(Optional.of(spacesCustomOrder));
+        serviceToTest.editSpacesCustomOrder(spaceCustomOrderId1, spaceIdList);
+        Mockito.verify(spacesCustomOrderRepository).save(spacesCustomOrder);
+    }
     
-
-   
+    @Test
+    public void test_addSpacesToCustomOrders_Success() {
+        Space space = new Space();
+        List<SpacesCustomOrder> spacesCustomOrders = new ArrayList<SpacesCustomOrder>();
+        when(spacesCustomOrderRepository.findAll()).thenReturn(spacesCustomOrders);
+        serviceToTest.addSpaceToCustomOrders(space);
+        Mockito.verify(spacesCustomOrderRepository).saveAll(spacesCustomOrders);
+        
+    }
+    
+    @Test
+    public void test_updateSpacesCustomOrderName_Success() {
+        String spaceId1 = "SPC001";
+        String spaceId2 = "SPC002";
+        List<String> spaceIdList = new ArrayList<String>();
+        spaceIdList.add(spaceId1);
+        spaceIdList.add(spaceId2);
+        SpacesCustomOrder spacesCustomOrder = new SpacesCustomOrder();
+        spacesCustomOrder.setCustomOrderName("hello");
+        when(spacesCustomOrderRepository.findById(spaceCustomOrderId1)).thenReturn(Optional.of(spacesCustomOrder));
+        serviceToTest.updateSpacesCustomOrderName(spaceCustomOrderId2, "updated");
+        Mockito.verify(spacesCustomOrderRepository).save(spacesCustomOrder);
+    }
+    
+    @Test
+    public void test_updateSpacesCustomOrderDescription_Success() {
+        String spaceId1 = "SPC001";
+        String spaceId2 = "SPC002";
+        List<String> spaceIdList = new ArrayList<String>();
+        spaceIdList.add(spaceId1);
+        spaceIdList.add(spaceId2);
+        SpacesCustomOrder spacesCustomOrder = new SpacesCustomOrder();
+        spacesCustomOrder.setCustomOrderName("hello");
+        when(spacesCustomOrderRepository.findById(spaceCustomOrderId1)).thenReturn(Optional.of(spacesCustomOrder));
+        serviceToTest.updateSpacesCustomOrderDescription(spaceCustomOrderId2, "updated");
+        Mockito.verify(spacesCustomOrderRepository).save(spacesCustomOrder);
+    }
     
 }
