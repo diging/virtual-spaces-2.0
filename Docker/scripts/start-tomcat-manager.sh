1f7b08b3c0624485b6f21452c942445972a697e2
Sola Kim <87000237+solablue@users.noreply.github.com>
2022-03-25 10:52:11 -0700

    Delete Docker directory

--- Docker/scripts/start-tomcat-manager.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo $TOMCAT_USERNAME
-
-sed -i 's/<pw-change-me>/'"$TOMCAT_PASSWORD"'/g' /usr/local/tomcat/conf/tomcat-users.xml
-sed -i 's/<username-change-me>/'"$TOMCAT_USERNAME"'/g' /usr/local/tomcat/conf/tomcat-users.xml
-
-/usr/local/tomcat/bin/catalina.sh run
