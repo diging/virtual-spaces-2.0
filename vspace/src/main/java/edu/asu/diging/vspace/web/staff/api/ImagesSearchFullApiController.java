71465248370b35506f748340773a80ad9f216e37
swetalina-prusty25 <100608518+swetalina-prusty25@users.noreply.github.com>
2022-04-13 16:30:52 -0700

    Update ImagesSearchFullApiController.java

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/ImagesSearchFullApiController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/ImagesSearchFullApiController.java
@@ -17,56 +17,55 @@ import edu.asu.diging.vspace.core.services.ISpaceManager;
 
 @Controller
 public class ImagesSearchFullApiController {
-		
-	@Autowired
+
+    @Autowired
     private IImageService imageService;
-    
+
     @Autowired
     private ISpaceManager spaceManager;
-    
-	
-	
-	  @RequestMapping("/staff/images/search/full") 
-	  public String imageSearchFull(Model model ,@RequestParam(value = "searchText", required = false) String searchTerm) { 
-		  return String.format("redirect:/staff/images/search/full/1?searchText=%s"); }
-	 
-	 
-    
+
+
+
+    @RequestMapping("/staff/images/search/full")
+    public String imageSearchFull(Model model, @RequestParam(value = "searchText", required = false) String searchTerm) {
+        return String.format("redirect:/staff/images/search/full/1?searchText=%s");
+    }
+
+
     @RequestMapping("/staff/images/search/full/{page}")
     public String imageSeacrhDescription(@PathVariable(required = false) String page,
-            @RequestParam(value = "searchText", required = false) String searchTerm , Model model, RedirectAttributes attributes){
-    	
-    	List<IVSImage> images = null;
+        @RequestParam(value = "searchText", required = false) String searchTerm, Model model, RedirectAttributes attributes) {
+
+        List < IVSImage > images = null;
         if (searchTerm != null && !searchTerm.trim().isEmpty()) {
-        	
+
             images = imageService.search(searchTerm);
         } else {
             images = imageService.getImages(1);
-        } 
+        }
         int pageNo;
         page = StringUtils.isEmpty(page) ? "1" : page;
-     
+
         try {
-            pageNo = imageService.validatePageNumber(Integer.parseInt(page),images);
-        } catch (NumberFormatException numberFormatException){
+            pageNo = imageService.validatePageNumber(Integer.parseInt(page), images);
+        } catch (NumberFormatException numberFormatException) {
             pageNo = 1;
         }
-        
+
         model.addAttribute("totalPages", imageService.getTotalPages(images));
         model.addAttribute("currentPageNumber", pageNo);
         model.addAttribute("totalImageCount", images.size());
-        List<IVSImage> imageResults = imageService.getImagesForPagination(pageNo, images, searchTerm);
-        Map<String, List<ISpace>> imageToSpaces = new HashMap<String, List<ISpace>>();
-        for(IVSImage image : imageResults) {
-            List<ISpace> spaces = spaceManager.getSpacesWithImageId(image.getId());
-            if(spaces!=null && spaces.size()>0) {
+        List < IVSImage > imageResults = imageService.getImagesForPagination(pageNo, images, searchTerm);
+        Map < String, List < ISpace >> imageToSpaces = new HashMap < String, List < ISpace >> ();
+        for (IVSImage image: imageResults) {
+            List < ISpace > spaces = spaceManager.getSpacesWithImageId(image.getId());
+            if (spaces != null && spaces.size() > 0) {
                 imageToSpaces.put(image.getId(), spaces);
             }
         }
-        model.addAttribute("imageToSpaces",imageToSpaces);
+        model.addAttribute("imageToSpaces", imageToSpaces);
         model.addAttribute("images", imageResults);
         model.addAttribute("searchTerm", searchTerm);
         return "staff/images/searchImages";
     }
 }
-   
