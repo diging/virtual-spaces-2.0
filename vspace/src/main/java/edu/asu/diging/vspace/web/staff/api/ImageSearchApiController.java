6a90e67222700a23a2753f28b7bbd05a4455a5a5
swetalina-prusty25 <100608518+swetalina-prusty25@users.noreply.github.com>
2022-04-13 16:14:53 -0700

    Update ImageSearchApiController.java

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/ImageSearchApiController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/ImageSearchApiController.java
@@ -1,5 +1,4 @@
 package edu.asu.diging.vspace.web.staff.api;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import edu.asu.diging.vspace.core.model.IVSImage;
 import edu.asu.diging.vspace.core.services.IImageService;
 
