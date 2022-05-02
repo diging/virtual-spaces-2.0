3a73b43f2ad8295396b3ee50b7c5b33f60529306
swetalina-prusty25 <100608518+swetalina-prusty25@users.noreply.github.com>
2022-05-02 09:10:21 -0700

    Update ModuleController.java

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/ModuleController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/ModuleController.java
@@ -1,14 +1,11 @@
 package edu.asu.diging.vspace.web.staff;
-
 import java.util.HashSet;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import edu.asu.diging.vspace.core.model.IModule;
 import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.model.impl.ModuleLink;
