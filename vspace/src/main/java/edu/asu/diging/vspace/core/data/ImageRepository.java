1da862537fa694bbb33defe10b87c285c6bcdb8f
swetalina-prusty25 <100608518+swetalina-prusty25@users.noreply.github.com>
2022-05-06 15:20:18 -0700

    Update ImageRepository.java

--- vspace/src/main/java/edu/asu/diging/vspace/core/data/ImageRepository.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/data/ImageRepository.java
@@ -1,10 +1,12 @@
 package edu.asu.diging.vspace.core.data;
 import java.util.List;
+
 import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
 import edu.asu.diging.vspace.core.model.IVSImage;
 import edu.asu.diging.vspace.core.model.ImageCategory;
 import edu.asu.diging.vspace.core.model.impl.VSImage;
@@ -24,6 +26,6 @@ public interface ImageRepository extends PagingAndSortingRepository<VSImage, Str
     Page<VSImage> findByCategories(Pageable pageable, ImageCategory category);
 
     List<IVSImage> findByCategories(ImageCategory category);
-    
-    long countByCategories(ImageCategory category);   
+        
+    long countByCategories(ImageCategory category);  
 }
