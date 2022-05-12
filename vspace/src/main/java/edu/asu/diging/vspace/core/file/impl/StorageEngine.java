068417423eac81b087f01d47ef23ac27600f2e38
glend17 <glendsouza1702@gmail.com>
2022-05-12 13:34:14 -0700

    [VSPC-202] Indentation fix

--- vspace/src/main/java/edu/asu/diging/vspace/core/file/impl/StorageEngine.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/file/impl/StorageEngine.java
@@ -23,115 +23,115 @@ import edu.asu.diging.vspace.core.model.IVSImage;
 @PropertySource({"classpath:config.properties", "${appConfigFile:classpath:}/app.properties"})
 public class StorageEngine implements IStorageEngine {
 
-	@Value("${uploads_path}")
-	private String path;
+    @Value("${uploads_path}")
+    private String path;
 
-	@Value("${file_uploads_directory}")
-	private String fileUploadDir;
+    @Value("${file_uploads_directory}")
+    private String fileUploadDir;
 
-	/* (non-Javadoc)
-	 * @see edu.asu.diging.vspace.core.file.impl.IStorageEngine#storeFile(byte[], java.lang.String, java.lang.String)
-	 */
-	@Override
-	public String storeFile(byte[] fileContent, String filename, String directory) throws FileStorageException {
-		File parent = new File(path + File.separator + directory);
-		if (!parent.exists()) {
-			parent.mkdir();
-		}
-		File file = new File(parent.getAbsolutePath() + File.separator + filename);
-		BufferedOutputStream stream;
-		try {
-			stream = new BufferedOutputStream(new FileOutputStream(file));
-		} catch (FileNotFoundException e) {
-			throw new FileStorageException("Could not store file.", e);
-		}
-		try {
-			stream.write(fileContent);
-			stream.close();
-		} catch (IOException e) {
-			throw new FileStorageException("Could not store file.", e);
-		}
+    /* (non-Javadoc)
+    * @see edu.asu.diging.vspace.core.file.impl.IStorageEngine#storeFile(byte[], java.lang.String, java.lang.String)
+    */
+    @Override
+    public String storeFile(byte[] fileContent, String filename, String directory) throws FileStorageException {
+        File parent = new File(path + File.separator + directory);
+	if (!parent.exists()) {
+            parent.mkdir();
+        }
+        File file = new File(parent.getAbsolutePath() + File.separator + filename);
+        BufferedOutputStream stream;
+        try {
+            stream = new BufferedOutputStream(new FileOutputStream(file));
+        } catch (FileNotFoundException e) {
+	    throw new FileStorageException("Could not store file.", e);
+        }
+        try {
+            stream.write(fileContent);
+            stream.close();
+        } catch (IOException e) {
+            throw new FileStorageException("Could not store file.", e);
+        }
 
-		return directory;
-	}
+        return directory;
+    }
 
-	@Override
-	public byte[] getImageContent(String directory, String filename) throws IOException {
-		File fileObject = new File(path + File.separator + directory + File.separator + filename);
-		URLConnection con = fileObject.toURI().toURL().openConnection();
+    @Override
+    public byte[] getImageContent(String directory, String filename) throws IOException {
+        File fileObject = new File(path + File.separator + directory + File.separator + filename);
+        URLConnection con = fileObject.toURI().toURL().openConnection();
 
-		InputStream input = con.getInputStream();
+        InputStream input = con.getInputStream();
 
-		byte[] buffer = new byte[4096];
+        byte[] buffer = new byte[4096];
 
-		ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-		BufferedOutputStream output = new BufferedOutputStream(byteOutput);
+        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+        BufferedOutputStream output = new BufferedOutputStream(byteOutput);
 
-		int n = -1;
-		while ((n = input.read(buffer)) != -1) {
-		    output.write(buffer, 0, n);
-		}
-		input.close();
-		output.flush();
-		output.close();
+        int n = -1;
+        while ((n = input.read(buffer)) != -1) {
+            output.write(buffer, 0, n);
+        }
+        input.close();
+        output.flush();
+        output.close();
 
-		byteOutput.flush();
-		byte[] bytes = byteOutput.toByteArray();
-		byteOutput.close();
-		return bytes;
-	}
+        byteOutput.flush();
+        byte[] bytes = byteOutput.toByteArray();
+        byteOutput.close();
+        return bytes;
+    }
 	
-	/**
-	* Method to rename image   
-	* 
-	* @param image - image file
-	* @param newFileName - new name of the file
-	* @return true if file renaming was successful, otherwise return false 
-	*/ 
-	@Override
-	public boolean renameImage(IVSImage image, String newFileName) {
-		File currentFile = new File(path + File.separator + image.getId() + File.separator + image.getFilename());
-		File renamedFile = new File(path + File.separator + image.getId() + File.separator + newFileName);
-		return currentFile.renameTo(renamedFile);
-	}
+    /**
+    * Method to rename image   
+    * 
+    * @param image - image file
+    * @param newFileName - new name of the file
+    * @return true if file renaming was successful, otherwise return false 
+    */ 
+    @Override
+    public boolean renameImage(IVSImage image, String newFileName) {
+        File currentFile = new File(path + File.separator + image.getId() + File.separator + image.getFilename());
+        File renamedFile = new File(path + File.separator + image.getId() + File.separator + newFileName);
+        return currentFile.renameTo(renamedFile);
+    }
 
-	@Override
-	public boolean renameFile(IVSFile file, String newFileName) {
-		File currentFile = new File(path + File.separator + fileUploadDir + File.separator + file.getFilename());
-		File renamedFile = new File(path + File.separator + fileUploadDir + File.separator + newFileName);
-		return currentFile.renameTo(renamedFile);
-	}
+    @Override
+    public boolean renameFile(IVSFile file, String newFileName) {
+        File currentFile = new File(path + File.separator + fileUploadDir + File.separator + file.getFilename());
+        File renamedFile = new File(path + File.separator + fileUploadDir + File.separator + newFileName);
+        r.eturn currentFile.renameTo(renamedFile);
+    }
 
-	@Override
-	public byte[] downloadFile(String directory, String filename) throws IOException {
-		File fileObject = new File(path + File.separator + fileUploadDir + File.separator + filename);
-		URLConnection con = fileObject.toURI().toURL().openConnection();
+    @Override
+    public byte[] downloadFile(String directory, String filename) throws IOException {
+        File fileObject = new File(path + File.separator + fileUploadDir + File.separator + filename);
+        URLConnection con = fileObject.toURI().toURL().openConnection();
 
-		InputStream input = con.getInputStream();
+        InputStream input = con.getInputStream();
 
-		byte[] buffer = new byte[4096];
+        byte[] buffer = new byte[4096];
 
-		ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
-		BufferedOutputStream output = new BufferedOutputStream(byteOutput);
+        ByteArrayOutputStream byteOutput = new ByteArrayOutputStream();
+        BufferedOutputStream output = new BufferedOutputStream(byteOutput);
 
-		int n = -1;
-		while ((n = input.read(buffer)) != -1) {
-		    output.write(buffer, 0, n);
-		}
-		input.close();
-		output.flush();
-		output.close();
+        int n = -1;
+        while ((n = input.read(buffer)) != -1) {
+            output.write(buffer, 0, n);
+        }
+        input.close();
+        output.flush();
+        output.close();
 
-		byteOutput.flush();
-		byte[] bytes = byteOutput.toByteArray();
-		byteOutput.close();
-		byte[] encodedBytes = Base64.getEncoder().encode(bytes);
-		return encodedBytes;
-	}
+        byteOutput.flush();
+        byte[] bytes = byteOutput.toByteArray();
+        byteOutput.close();
+        byte[] encodedBytes = Base64.getEncoder().encode(bytes);
+        return encodedBytes;
+    }
 
-	@Override
-	public boolean deleteFile(IVSFile file) {
-		File storedFile = new File(path + File.separator + fileUploadDir + File.separator + file.getFilename());
-		return storedFile.delete();
-	}
+    @Override
+    public boolean deleteFile(IVSFile file) {
+        File storedFile = new File(path + File.separator + fileUploadDir + File.separator + file.getFilename());
+        return storedFile.delete();
+    }
 }
