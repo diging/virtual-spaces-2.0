3f16a41e4d3f70e6046f3d20cfd6a83ab6fd3893
Prachi Kharge <prachikharge@Prachis-MacBook-Air.local>
2022-06-13 16:20:58 -0700

    [story/VSPC-202] download button

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/DownloadFileController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/DownloadFileController.java
@@ -1,8 +1,11 @@
 package edu.asu.diging.vspace.web.staff;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -30,4 +33,21 @@ public class DownloadFileController {
         
         return new ResponseEntity<byte[]>(fileContent, HttpStatus.OK);
     }
+    
+    
+//  @RequestMapping(value = "/staff/files/download/{fileId}", method = RequestMethod.GET)
+//  public ResponseEntity<FileSystemResource> downloadFile(Model model, @PathVariable String fileId) {
+//      FileSystemResource fileContent = null;
+//      try {
+//          File file = new File(fileId);
+//        FileUtils.writeByteArrayToFile(file, fileManager.downloadFile(fileId));;
+//          fileContent = new FileSystemResource(file);
+//          
+////          fileManager.downloadFile(fileId);
+//      } catch (IOException e) {
+//          return new ResponseEntity<FileSystemResource>(fileContent, HttpStatus.INTERNAL_SERVER_ERROR);
+//      }
+//      
+//      return new ResponseEntity<FileSystemResource>(fileContent, HttpStatus.OK);
+//  }
 }
