2cdd59ce8402785eeaf36a84e317cc542126e2d4
swetalina-prusty25 <sprusty1@asu.edu>
2022-12-15 16:21:32 -0700

    [bug/VSPC-232] Implemented the story

--- vspace/src/main/java/edu/asu/diging/vspace/core/model/IExhibitionAboutPage.java
+++
@@ -1,8 +0,0 @@
-package edu.asu.diging.vspace.core.model;
-
-
-public interface IExhibitionAboutPage{
-	public void setAboutPageDescription(String aboutPageTexts);
-	public void setAboutPageTitle(String title);
-
-}
