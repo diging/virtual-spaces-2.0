de83468b4229654b2c8979f734d48510fe7bc099
Baishali Nayak <bnayak4@asu.edu>
2023-03-30 13:17:13 -0700

    [VSPC-150] addressed review comment

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/ReferenceController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/ReferenceController.java
@@ -8,19 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import edu.asu.diging.vspace.core.model.IBiblioBlock;
 import edu.asu.diging.vspace.core.model.IReference;
-import edu.asu.diging.vspace.core.model.impl.Reference;
-import edu.asu.diging.vspace.core.references.IReferenceDisplayProvider;
-import edu.asu.diging.vspace.core.services.IContentBlockManager;
 import edu.asu.diging.vspace.core.services.IReferenceManager;
-import edu.asu.diging.vspace.web.model.ReferenceBlock;
 
 @Controller
 public class ReferenceController {
@@ -28,11 +19,7 @@ public class ReferenceController {
     @Autowired
     private IReferenceManager referenceManager;
     
-    @Autowired
-    private IContentBlockManager contentBlockManager;
     
-    @Autowired
-    private IReferenceDisplayProvider referenceDisplayProvider;
 
     @RequestMapping("/staff/display/reference/{id}")
     public String showReference(@PathVariable String id, Model model) {
@@ -51,14 +38,4 @@ public class ReferenceController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     
-    @RequestMapping(value = "/staff/module/{id}/slide/{slideId}/biblio/{biblioId}/reference/add", method = RequestMethod.POST)
-    public ResponseEntity<ReferenceBlock> addReference(@PathVariable("id") String moduleId, @PathVariable("slideId") String slideId, 
-            @PathVariable("biblioId") String biblioId, 
-            @RequestBody Reference reference) throws JsonProcessingException {
-        IBiblioBlock biblio = contentBlockManager.getBiblioBlock(biblioId);
-        IReference ref = referenceManager.saveReference(biblio, reference);
-        String refDisplayText = referenceDisplayProvider.getReferenceDisplayText(ref);
-        ReferenceBlock refBlock = new ReferenceBlock(ref, refDisplayText);
-        return new ResponseEntity<ReferenceBlock>(refBlock, HttpStatus.OK);
-    }
 }
