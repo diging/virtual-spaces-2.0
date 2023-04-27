d02049eea1727ee1c06c5bb95e5f5f85fbbe3b1c
Baishali Nayak <bnayak4@asu.edu>
2023-04-27 11:44:04 -0700

    [VSPC-244] addressed review comments

--- vspace/src/test/java/edu/asu/diging/vspace/config/ExhibitionLanguageConfigTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/config/ExhibitionLanguageConfigTest.java
@@ -26,22 +26,21 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ExhibitionLanguageConfigTest {
-
-    @InjectMocks
-    private ExhibitionLanguageConfig exhibitionLanguageConfig;
-
     @Mock
     private AbstractEnvironment environment;
+
+    @InjectMocks
+    private ExhibitionLanguageConfig serviceToTest;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        exhibitionLanguageConfig = new ExhibitionLanguageConfig();
-        ReflectionTestUtils.setField(exhibitionLanguageConfig, "environment", environment);
+        serviceToTest = new ExhibitionLanguageConfig();
+        ReflectionTestUtils.setField(serviceToTest, "environment", environment);
     }
 
     @Test
-    public void testInit() {
+    public void test_init_success() {
         // Set up test data
         List<Map> expectedExhibitionLanguageList = new ArrayList<Map>();
         Map<String, Object> languageMap = new HashMap<String, Object>();
@@ -54,34 +53,34 @@ public class ExhibitionLanguageConfigTest {
         propertySources.addFirst(propertySource);
         when(environment.getPropertySources()).thenReturn(propertySources);
 
-        exhibitionLanguageConfig.init();
+        serviceToTest.init();
 
-        List<Map> actualExhibitionLanguageList = (List<Map>) ReflectionTestUtils.getField(exhibitionLanguageConfig,
+        List<Map> actualExhibitionLanguageList = (List<Map>) ReflectionTestUtils.getField(serviceToTest,
                 "exhibitionLanguageList");
         assertSame(expectedExhibitionLanguageList, actualExhibitionLanguageList);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testInitWithNullPropertySource() {
+    public void test_init_withNullPropertySource() {
         // Define the mock behavior for the environment
 
         when(environment.getPropertySources()).thenReturn(null);
         // Call the init method on the ExhibitionLanguageConfig object
-        exhibitionLanguageConfig.init();
+        serviceToTest.init();
 
         // Verify that the exhibitionLanguageList property is empty
-        assertNotNull(exhibitionLanguageConfig.getExhibitionLanguageList());
-        assertTrue(exhibitionLanguageConfig.getExhibitionLanguageList().isEmpty());
+        assertNotNull(serviceToTest.getExhibitionLanguageList());
+        assertTrue(serviceToTest.getExhibitionLanguageList().isEmpty());
     }
 
     @Test
-    public void testSetterAndGetter() {
+    public void test_getExhibitionLanguageList_success() {
         List<Map> languageList = Arrays.asList(Map.of("key1", "value1"), Map.of("key2", "value2"));
 
-        exhibitionLanguageConfig.setExhibitionLanguageList(languageList);
+        serviceToTest.setExhibitionLanguageList(languageList);
 
-        assertNotNull(exhibitionLanguageConfig.getExhibitionLanguageList());
-        assertEquals(languageList, exhibitionLanguageConfig.getExhibitionLanguageList());
+        assertNotNull(serviceToTest.getExhibitionLanguageList());
+        assertEquals(languageList, serviceToTest.getExhibitionLanguageList());
     }
 
 }
