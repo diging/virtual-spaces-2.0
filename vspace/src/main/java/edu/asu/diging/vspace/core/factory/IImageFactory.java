767b9505fed839218563de0d3cc2898b09a37a6a
Baishali Nayak <bnayak4@asu.edu>
2023-05-01 14:44:24 -0700

    [VSPC-251] ticket done

--- vspace/src/main/java/edu/asu/diging/vspace/core/factory/IImageFactory.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/factory/IImageFactory.java
@@ -2,8 +2,24 @@ package edu.asu.diging.vspace.core.factory;
 
 import edu.asu.diging.vspace.core.model.IVSImage;
 
+/**
+ * The IImageFactory interface defines a factory for creating instances of the {@link IVSImage}
+ * interface. 
+ * Implementations of this interface should provide an implementation for the
+ * {@link IImageFactory#createImage(java.lang.String, java.lang.String)} method, which takes a filename and file type
+ * as input parameters and returns an instance of the edu.asu.diging.vspace.core.model.IVSImage interface.
+ * 
+ */
 public interface IImageFactory {
 
+    /**
+     * Creates a new instance of the {@link IVSImage} interface, based on the given filename and
+     * file type.
+     * 
+     * @param filename The filename of the image to create.
+     * @param fileType The file type of the image to create.
+     * @return An instance of the edu.asu.diging.vspace.core.model.IVSImage interface.
+     */
     IVSImage createImage(String filename, String fileType);
 
 }
