2552dcdfedf2292bd83862cd181d4a03c9500080
Julia Damerow <jdamerow@users.noreply.github.com>
2023-05-12 08:58:49 -0400

    [VSPC-243] formatting fixes

--- vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ExhibitionManagerTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/ExhibitionManagerTest.java
@@ -61,7 +61,6 @@ public class ExhibitionManagerTest {
         Exhibition exhibition = new Exhibition();
         exhibition.setId(id);
         Optional<Exhibition> exhibitionOptional = Optional.of(exhibition);
-        ;
         when(exhibitRepo.findById(id)).thenReturn(exhibitionOptional);
 
         IExhibition exhibitionTest = serviceToTest.getExhibitionById(id);
@@ -176,7 +175,6 @@ public class ExhibitionManagerTest {
         exhibition.getLanguages().forEach(language -> {
             if (language.getCode().equals("en")) {
                 assertTrue(language.isDefault());
-
             }
         });
 
@@ -185,11 +183,9 @@ public class ExhibitionManagerTest {
         exhibition.getLanguages().forEach(language -> {
             if (language.getCode().equals("en")) {
                 assertFalse(language.isDefault());
-
             }
             if (language.getCode().equals("aa")) {
                 assertTrue(language.isDefault());
-
             }
         });
 
