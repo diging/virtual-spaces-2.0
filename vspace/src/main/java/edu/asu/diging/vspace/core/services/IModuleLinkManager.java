8a9d4d31eb42aa4d7e6356be746f64c88315a0b3
swetalina-prusty25 <sprusty1@asu.edu>
2023-05-24 16:21:42 -0700

    [bug/VSPC-218] Linked space page

--- vspace/src/main/java/edu/asu/diging/vspace/core/services/IModuleLinkManager.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/IModuleLinkManager.java
@@ -7,7 +7,7 @@ import edu.asu.diging.vspace.core.model.IModuleLink;
 import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.model.display.IModuleLinkDisplay;
 
-public interface IModuleLinkManager extends ILinkManager <IModuleLink, IModule, IModuleLinkDisplay> {
+public interface IModuleLinkManager extends ILinkManager<IModuleLink, IModule, IModuleLinkDisplay> {
 
     Set<ISpace> findSpaceListFromModuleId(String moduleId);
 
