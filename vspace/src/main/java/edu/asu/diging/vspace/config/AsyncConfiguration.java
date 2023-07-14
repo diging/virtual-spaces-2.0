f777e5330dce64b134f02095690810270ea4eaa0
swetalina-prusty25 <sprusty1@asu.edu>
2023-07-14 16:08:13 -0700

    [story/VSPC-204]Getting string value

--- vspace/src/main/java/edu/asu/diging/vspace/config/AsyncConfiguration.java
+++ vspace/src/main/java/edu/asu/diging/vspace/config/AsyncConfiguration.java
@@ -4,40 +4,50 @@ import java.util.concurrent.Executor;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@PropertySource("classpath:config.properties")
 public class AsyncConfiguration implements AsyncConfigurer  {
 
     @Value("${async_core_pool_size}")
-    private String corePoolSize;
+    private int corePoolSize;
     
     @Value("${async_max_pool_size}")
-    private String maxPoolSize;
+    private int maxPoolSize;
     
     @Value("${async_queue_capacity}")
-    private String queueCapacity;
+    private int queueCapacity;
+    
     
     
     @Bean(name="asyncExecutor")
     @Override
     public Executor getAsyncExecutor() {
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(new Integer(corePoolSize));
-        executor.setMaxPoolSize(new Integer(maxPoolSize));
-        executor.setQueueCapacity(new Integer(queueCapacity));
-        executor.setThreadNamePrefix("threadAsync");
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.initialize();
         
-        return executor;
+        System.out.println("size = "+corePoolSize);
+        
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+        ThreadPoolTaskExecutor asyncExecutor = new ThreadPoolTaskExecutor();
+        
+        asyncExecutor.setCorePoolSize(corePoolSize);
+        asyncExecutor.setMaxPoolSize(maxPoolSize);
+        asyncExecutor.setQueueCapacity(queueCapacity);
+        
+        asyncExecutor.setThreadNamePrefix("threadAsync");
+        asyncExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        asyncExecutor.initialize();
+        
+        return asyncExecutor;
     }
     
 }
