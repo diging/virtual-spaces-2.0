876f41139d4fad698711a4f7f6fe4560a5784d5b
swetalina-prusty25 <sprusty1@asu.edu>
2023-07-25 15:24:39 -0700

    [VSPC-264] Tests

--- vspace/src/test/java/edu/asu/diging/vspace/core/factory/impl/SpaceLinkFactoryTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/core/factory/impl/SpaceLinkFactoryTest.java
@@ -2,8 +2,17 @@ package edu.asu.diging.vspace.core.factory.impl;
 
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.ISpaceLink;
+import edu.asu.diging.vspace.core.model.impl.SpaceLink;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -12,9 +21,22 @@ public class SpaceLinkFactoryTest {
     @InjectMocks
     private SpaceLinkFactory spaceLinkFactory = new SpaceLinkFactory();
     
+    @Mock
+    private ISpace sourceSpace;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
     @Test
     public void test_createSpaceLink_success() {
+   
+        String title = "Test Space";
+        ISpaceLink link = spaceLinkFactory.createSpaceLink(title, sourceSpace);
         
+        assertEquals(title, link.getName());
+        assertEquals(sourceSpace, link.getSpace());
     }
     
     
