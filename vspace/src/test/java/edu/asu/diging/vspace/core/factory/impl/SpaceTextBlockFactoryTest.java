1db683faaca47a5e1a37a5887e559e21eab1a2e7
swetalina-prusty25 <sprusty1@asu.edu>
2023-07-26 16:08:33 -0700

    [VSPC-266] Tests and java doc

---
+++ vspace/src/test/java/edu/asu/diging/vspace/core/factory/impl/SpaceTextBlockFactoryTest.java
@@ -0,0 +1,40 @@
+package edu.asu.diging.vspace.core.factory.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.ISpaceTextBlock;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SpaceTextBlockFactoryTest {
+    @InjectMocks
+    private SpaceTextBlockFactory spaceTextBlockFactory = new SpaceTextBlockFactory();
+    
+    @Mock
+    private ISpace space;
+    
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void test_createSpaceLink_success() {
+   
+        String text = "Test SpaceTextBlock";
+        ISpaceTextBlock spaceTextBlock  = spaceTextBlockFactory.createSpaceTextBlock(text, space);
+        
+        assertEquals(text, spaceTextBlock.getName());
+        assertEquals(space, spaceTextBlock.getSpace());
+    }
+ 
+        
+}
