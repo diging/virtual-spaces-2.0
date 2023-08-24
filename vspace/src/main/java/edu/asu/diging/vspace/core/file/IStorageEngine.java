ab5e20c945dc42a8d53c52b90a3b853f9a019933
glend17 <glendsouza1702@gmail.com>
2023-08-24 08:24:54 -0700

    Story/vspc 202 Upload files (#298)
    * [VSPC-202] File init
    
    * [VSPC-202] File Api's
    
    * [VSPC-202] backend changes
    
    * [VSPC-202] Frontend changes
    
    * [VSPC-202] File Form
    
    * [VSPC-202] File edit
    
    * [VSPC-202] Download file
    
    * [VSPC-202] Downloading, Renaming and deletion of files
    
    * [VSPC-202] file delete and ui changes
    
    * [VSPC-202] Unit tests and refactor
    
    * [VSPC-202] refactor
    
    * [VSPC-202] Indentation fix
    
    * [VSPC-202] Indentation fix
    
    * [VSPC-202] Indentation fix
    
    * [VSPC-202] Indentation fix
    
    * [VSPC-202] indentation fix
    
    * [VSPC-202] Indentation fix
    
    * [story/VSPC-204] addressed review comments
    
    * [story/VSPC-202] code factor
    
    * [story/VSPC-202] download button
    
    * [story/VSPC-202] headers in download file
    
    * [story/VSPC-202] download file without javascript
    
    * [story/VSPC-202] test case name change
    
    * [story/VSPC-202]
    
    * [story/VSPC-202] fixed test case
    
    * [story/VSPC-202] modified path to maintenance html file
    
    * [story/VSPC-202] removed unwanted imports
    
    * [story/VSPC-202] code factor
    
    * [story/VSPC-202] code factor
    
    * [story/VSPC-202] code factor
    
    * [story/VSPC-202] modified IVSImage to extend IVSFile
    
    * [story/VSPC-202] ivsimage and ivsfile
    
    * [story/VSPC-202] added IVSMedia and VSMedia mapped super class
    
    * [story/VSPC-202] parentpath
    
    * [story/VSPC-202] refactor
    
    * [story/VSPC-202] fixed test cases
    
    * [story/VSPC-202] review comments
    
    * [story/VSPC-202] add file api modified
    
    * [story/VSPC-202] delete file api response
    
    * [story/VSPC-202] removed unwanted imports
    
    * [story/VSPC-202] test case
    
    * [story/VSPC-202] code factor
    
    * [story/VSPC-202] removed unwanted imports
    
    * [story/VSPC-202] review comment
    
    * [story/VSPC-202] ui changes
    
    * [story/VSPC-202] icon
    
    * [story/VSPC-202] addressed review comments
    
    * [story/VSPC-202]review comments
    
    * [story/VSPC-202] pagination backend
    
    * [story/VSPC-202] paginagte
    
    * [story/VSPC-202] review comment
    
    * [story/VSPC-202] removed unwanted changes
    
    * [story/VSPC-202]
    
    * [story/VSPC-202]
    
    * [story/VSPC]
    
    * [story/VSPC-202]
    
    * [story/VSPC-202] fixed test cases
    
    * [story/VSPC-202] page number
    
    * [story/VSPC-202] review comments
    
    * [story/VSPC-202] edit api
    
    * [story/VSPC-202] review comment
    
    * [story/VSPC-202] download file and delete file ui
    
    * [story/VSPC-202] test case
    
    * [story/VSPC-202] test case
    
    * [story/VSPC-202] redirect from edit file
    
    * [story/VSPC-202] file upload success message
    
    * [story/VSPC-202] conflicts resolved
    
    * [story/VSPC-202] refactor
    
    * [story/VSPC-202 ] alerts thymeleaf
    
    * [story/VSPC-202] javascript alert
    
    * [story/VSPC-202]
    
    * [story/VSPC-202] review comments
    
    * [story/VSPC-202] review comment
    
    * [story/VSPC-202] review comments
    
    ---------
    
    Co-authored-by: Prachi Kharge <prachikharge@Prachis-MacBook-Air.local>
    Co-authored-by: pkharge <pkharge@asu.edu>

--- vspace/src/main/java/edu/asu/diging/vspace/core/file/IStorageEngine.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/file/IStorageEngine.java
@@ -1,6 +1,9 @@
 package edu.asu.diging.vspace.core.file;
 
+import java.io.File;
 import java.io.IOException;
+
+import org.springframework.core.io.Resource;
 
 import edu.asu.diging.vspace.core.exception.FileStorageException;
 import edu.asu.diging.vspace.core.model.IVSImage;
@@ -9,7 +12,18 @@ public interface IStorageEngine {
 
     String storeFile(byte[] fileContent, String filename, String directory) throws FileStorageException;
 
+    Resource downloadFile(String fileName, String directory) throws IOException;
+
+    boolean renameFile(String fileName, String newFileName, String directory);
+
+    boolean deleteFile(String fileName, String directory);
+
+    File getFile(String fileUploadDir, String fileName);
+    
+    File getDirectoryPath(String directory);
+
     byte[] getMediaContent(String directory, String filename) throws IOException;
 
     boolean renameImage(IVSImage image, String newFileName);
 }
+
---
+++ vspace/src/test/java/edu/asu/diging/vspace/core/services/impl/FileManagerTest.java
@@ -0,0 +1,203 @@
+package edu.asu.diging.vspace.core.services.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import edu.asu.diging.vspace.core.data.FileRepository;
+import edu.asu.diging.vspace.core.exception.FileStorageException;
+import edu.asu.diging.vspace.core.factory.IFileFactory;
+import edu.asu.diging.vspace.core.file.IStorageEngine;
+import edu.asu.diging.vspace.core.model.IVSFile;
+import edu.asu.diging.vspace.core.model.impl.VSFile;
+import edu.asu.diging.vspace.core.model.impl.VSImage;
+
+public class FileManagerTest {
+    
+    @Mock
+    private IStorageEngine storageEngine;
+    
+    @Mock
+    private IFileFactory fileFactory;
+    
+    @Mock
+    private FileRepository fileRepo;
+    
+    @InjectMocks
+    private FileManager serviceToTest;
+    
+    final String fileId = "FILE_ID";
+    private final String fileName = "fileName";
+    private final String fileDescription = "fileName";
+    private final String fileContentString = "file content";
+       
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        ReflectionTestUtils.setField(serviceToTest, "pageSize", 10);
+
+    }
+    
+    @Test
+    public void test_getFileById_success() {
+        VSFile file = new VSFile();
+        file.setId(fileId);
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(file));
+        IVSFile fileResponse = serviceToTest.getFileById(fileId);
+        assertEquals(fileResponse.getId(), "FILE_ID");
+        
+    }
+    
+    @Test
+    public void test_getFileById_failure() {
+        VSFile file = new VSFile();
+        file.setId(fileId);
+        when(fileRepo.findById(fileId)).thenReturn(Optional.ofNullable(null));
+        IVSFile fileResponse = serviceToTest.getFileById(fileId);
+        assertEquals(fileResponse, null);
+        
+    }
+    
+    @Test
+    public void test_getAllFiles_success() {
+        List<VSFile> files = new ArrayList();
+        VSFile file = new VSFile();
+        file.setId(fileId);
+        files.add(file);
+        Pageable requestedPageForFiles = PageRequest.of(0, 10);
+        when(fileRepo.findAll(requestedPageForFiles)).thenReturn( new PageImpl<VSFile>(files));
+        Page<IVSFile> filesResponse = serviceToTest.getAllFiles(1);
+        assertEquals(filesResponse.getNumberOfElements(), 1);
+        assertEquals(filesResponse.getContent().get(0).getId(), fileId);
+    }
+    
+    @Test
+    public void test_getAllFiles_emptyResult() {
+        Pageable requestedPageForFiles = PageRequest.of(0, 10);
+        when(fileRepo.findAll(requestedPageForFiles)).thenReturn( new PageImpl<VSFile>(new ArrayList()));
+        Page<IVSFile> filesResponse = serviceToTest.getAllFiles(1);
+        assertEquals(filesResponse.getNumberOfElements(), 0);
+    }
+    
+    @Test
+    public void test_editFile_success() {
+        VSFile file = new VSFile();
+        file.setFilename(fileName);
+        file.setId(fileId);
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(file));
+        when(storageEngine.renameFile(file.getFilename(), "NEW_FILENAME", fileId)).thenReturn(true);
+        IVSFile returnedFile = serviceToTest.editFile(fileId, "NEW_FILENAME", fileDescription);                
+        assertEquals("NEW_FILENAME", returnedFile.getFilename());
+        assertEquals(fileDescription, returnedFile.getFileDescription());
+    }
+    
+    @Test
+    public void test_editFile_failure() {
+        VSFile file = new VSFile();
+        file.setFilename(fileName);
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(file));
+        when(storageEngine.renameFile(file.getFilename(), "NEW_FILENAME", fileId)).thenReturn(false);
+        IVSFile returnedFile = serviceToTest.editFile(fileId, "NEW_FILENAME", fileDescription);
+        assertNotEquals("NEW_FILENAME", returnedFile.getFilename());
+    }
+    
+    @Test
+    public void test_deleteFile_success() {
+        VSFile file = new VSFile();
+        file.setFilename(fileName);
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(file));
+        when(storageEngine.deleteFile(file.getFilename(), fileId)).thenReturn(true);
+        serviceToTest.deleteFile(fileId);
+        verify(fileRepo).delete(file);
+    }
+    
+    @Test
+    public void test_deleteFile_failure() {
+        VSFile file = new VSFile();
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(file));
+        when(storageEngine.deleteFile(file.getFilename(), "")).thenReturn(false);
+        assertFalse(serviceToTest.deleteFile(fileId));
+    }
+    
+    @Test
+    public void test_storeFile_success() throws FileStorageException {
+        String fileId = "fileId";
+        VSFile file = new VSFile();
+        file.setId(fileId);
+        String relativePath = "relativePath";
+        byte[] fileBytes = fileContentString.getBytes();
+        when(storageEngine.deleteFile(file.getFilename(), null)).thenReturn(true);
+        when(fileFactory.createFile(fileName, "text/plain")).thenReturn(file);
+        when(storageEngine.storeFile(fileBytes, fileName, fileContentString)).thenReturn(relativePath);
+        CreationReturnValue returnValue = serviceToTest.storeFile(fileBytes, fileName, fileDescription, fileId);
+        verify(fileFactory).createFile(fileName, "text/plain");
+        assertEquals(returnValue.getElement().getId(), fileId);
+    }
+    
+    @Test
+    public void test_storeFile_failure() throws FileStorageException {
+        
+        String fileId = "fileId";
+        VSFile file = new VSFile();
+        byte[] fileBytes = fileContentString.getBytes();
+        when(storageEngine.deleteFile(file.getFilename(), null)).thenReturn(true);
+        when(fileFactory.createFile(fileName, "text/plain")).thenReturn(file);
+        when(storageEngine.storeFile(fileBytes, fileName, null)).thenThrow(new FileStorageException());
+        CreationReturnValue returnValue = serviceToTest.storeFile(fileBytes, fileName, fileDescription, fileId);
+
+        Assert.assertTrue(returnValue.getErrorMsgs().get(0).contains("File could not be stored: "));
+    }
+   
+    @Test
+    public void test_downloadFile_success() throws IOException {
+        String fileId = "fileId";
+        VSFile vsFile = new VSFile();
+        when(storageEngine.downloadFile(Mockito.any(String.class), Mockito.any( String.class))).thenReturn(new ByteArrayResource(fileContentString.getBytes()));      
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(vsFile));
+        Resource fileResponse = serviceToTest.downloadFile("filename", fileId);
+        assertEquals(fileResponse ,new ByteArrayResource(fileContentString.getBytes()) );
+            
+    }
+    
+    @Test
+    public void test_downloadFile_failure() throws IOException {
+        String fileId = "fileId";
+        VSFile file = new VSFile();
+        when(fileRepo.findById(fileId)).thenReturn(Optional.of(file));
+        when(storageEngine.downloadFile(Mockito.any(String.class) ,Mockito.any( String.class))).thenThrow(new IOException());
+        assertThrows(IOException.class, () -> serviceToTest.downloadFile(file.getFilename(), fileId));
+    }
+}
