3c7e35207dad0155e8af51775062dc3c9f75759e
swetalina-prusty25 <sprusty1@asu.edu>
2023-10-18 16:28:44 -0700

    [story/VSPC-242] Manager methods

---
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/ISlideExternalLinkManager.java
@@ -0,0 +1,24 @@
+package edu.asu.diging.vspace.core.services;
+
+import edu.asu.diging.vspace.core.exception.ImageCouldNotBeStoredException;
+import edu.asu.diging.vspace.core.exception.LinkDoesNotExistsException;
+import edu.asu.diging.vspace.core.exception.SlideDoesNotExistException;
+import edu.asu.diging.vspace.core.model.IExternalLink;
+import edu.asu.diging.vspace.core.model.IExternalLinkSlide;
+import edu.asu.diging.vspace.core.model.display.DisplayType;
+import edu.asu.diging.vspace.core.model.display.ExternalLinkDisplayMode;
+import edu.asu.diging.vspace.core.model.display.IExternalLinkDisplay;
+import edu.asu.diging.vspace.core.model.impl.ExternalLinkValue;
+
+public interface ISlideExternalLinkManager extends ISlideLinkManager<IExternalLinkSlide, ExternalLinkValue, IExternalLinkDisplay>{
+    
+    IExternalLinkDisplay createLink(String title, String id, float positionX, float positionY, int rotation, String linkedId,
+            String linkLabel, DisplayType displayType, byte[] linkImage, String imageFilename,ExternalLinkDisplayMode howToOpen)
+            throws SlideDoesNotExistException, ImageCouldNotBeStoredException, SlideDoesNotExistException;
+
+    IExternalLinkDisplay updateLink(String title, String id, float positionX, float positionY, int rotation, String linkedId,
+            String linkLabel, String linkId, String linkDisplayId, DisplayType displayType, byte[] linkImage,
+            String imageFilename,ExternalLinkDisplayMode howToOpen)
+            throws SlideDoesNotExistException, LinkDoesNotExistsException, ImageCouldNotBeStoredException;
+
+}
