9f6ac3925b68c0ef27c6115360c8977813c5af29
swetalina-prusty25 <sprusty1@asu.edu>
2023-11-09 12:48:12 -0700

    [story/VSPC-242]Created slide display manager

---
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/ISlideDisplayManager.java
@@ -0,0 +1,9 @@
+package edu.asu.diging.vspace.core.services;
+
+import edu.asu.diging.vspace.core.model.ISpace;
+import edu.asu.diging.vspace.core.model.display.ISpaceDisplay;
+
+public interface ISlideDisplayManager {
+    ISlideDisplay getBySlide(ISlide slide);
+
+}
--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/AddSlideExternalLinkController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/AddSlideExternalLinkController.java
@@ -1,11 +1,13 @@
 package edu.asu.diging.vspace.web.staff;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +20,14 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.asu.diging.vspace.core.exception.ImageCouldNotBeStoredException;
 import edu.asu.diging.vspace.core.exception.SlideDoesNotExistException;
 import edu.asu.diging.vspace.core.exception.SpaceDoesNotExistException;
+import edu.asu.diging.vspace.core.model.IContentBlock;
 import edu.asu.diging.vspace.core.model.ISlide;
 import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.model.display.DisplayType;
 import edu.asu.diging.vspace.core.model.display.ExternalLinkDisplayMode;
 import edu.asu.diging.vspace.core.model.display.IExternalLinkDisplay;
 import edu.asu.diging.vspace.core.model.display.ISlideExternalLinkDisplay;
+import edu.asu.diging.vspace.core.services.IContentBlockManager;
 import edu.asu.diging.vspace.core.services.IExternalLinkManager;
 import edu.asu.diging.vspace.core.services.ISlideExternalLinkManager;
 import edu.asu.diging.vspace.core.services.ISlideManager;
@@ -37,6 +41,9 @@ public class AddSlideExternalLinkController {
 
     @Autowired
     private ISlideExternalLinkManager slideExternalLinkManager;
+    
+    @Autowired
+    private IContentBlockManager contentBlockManager;
     
     @RequestMapping(value = "/staff/module/{moduleId}/slide/{id}/externallink", method = RequestMethod.POST)
     public ResponseEntity<String> createSlideExternalLink(@PathVariable("id") String id, @PathVariable("moduleId") String moduleId, 
@@ -68,6 +75,8 @@ public class AddSlideExternalLinkController {
         linkNode.put("x", display.getPositionX());
         linkNode.put("y", display.getPositionY());
         linkNode.put("url", display.getExternalLink().getId());
+       
+        
 
         return new ResponseEntity<>(mapper.writeValueAsString(linkNode), HttpStatus.OK);
     }
