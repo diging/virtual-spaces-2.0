accfa4a3359760b73a2d89a03e5a29bd4227e521
ajay3568yadav <89062459+ajay3568yadav@users.noreply.github.com>
2024-03-29 16:42:49 -0700

    VSPC-171 review comments and image block

--- vspace/src/test/java/edu/asu/diging/vspace/config/ExhibitionLanguageConfigTest.java
+++ vspace/src/test/java/edu/asu/diging/vspace/config/ExhibitionLanguageConfigTest.java
@@ -77,12 +77,7 @@ public class ExhibitionLanguageConfigTest {
 
     @Test
     public void test_getExhibitionLanguageList_success() {
-        Map< String, String> map1 = new HashMap();
-        map1.put("key1", "value1") ;
-        Map< String, String> map2 = new HashMap();
-        map1.put("key2", "value2") ;
-        
-        List<Map> languageList = Arrays.asList(map1, map2);
+        List<Map> languageList = Arrays.asList(Map.of("key1", "value1"), Map.of("key2", "value2"));
 
         serviceToTest.setExhibitionLanguageList(languageList);
 
