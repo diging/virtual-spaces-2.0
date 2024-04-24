7f3ce5724f3cbcb3658823af51d6f48ca7b90dde
pooja-thalur <poojathalur@yahoo.com>
2024-04-24 12:35:30 -0700

    fixed the config issue and removed unused imports

--- vspace/src/main/java/edu/asu/diging/vspace/config/AsyncConfiguration.java
+++ vspace/src/main/java/edu/asu/diging/vspace/config/AsyncConfiguration.java
@@ -4,21 +4,15 @@ import java.util.concurrent.Executor;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableAsync
 @Configuration
-@PropertySource("classpath:config.properties")
-public class AsyncConfiguration implements AsyncConfigurer  {
-
+public class AsyncConfiguration {
+    
     @Value("${async_core_pool_size}")
     private int corePoolSize;
     
@@ -28,12 +22,8 @@ public class AsyncConfiguration implements AsyncConfigurer  {
     @Value("${async_queue_capacity}")
     private int queueCapacity;
     
-    
-    
     @Bean(name="asyncExecutor")
-    @Override
     public Executor getAsyncExecutor() {
-        
         System.out.println("size = "+corePoolSize);
         
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
