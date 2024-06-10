e12f6f0fa4c27eb94f7e22c60259041f4d525906
pooja-thalur <poojathalur@yahoo.com>
2024-06-10 08:35:32 -0700

    [VSPC-204] Fixed code factor issue

--- vspace/src/main/java/edu/asu/diging/vspace/core/exception/SnapshotCouldNotBeCreatedException.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/exception/SnapshotCouldNotBeCreatedException.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Snapshot could not be created")
 public class SnapshotCouldNotBeCreatedException extends Exception{
-private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     
     public SnapshotCouldNotBeCreatedException(String id) {
         super("Snapshot could not be created for snapshot task id " + id + " not found");
