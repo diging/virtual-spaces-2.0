d0c932fdcc5466670a025a608c8b420e494c24e3
pooja-thalur <poojathalur@yahoo.com>
2024-07-11 16:35:47 -0700

    [VSPC-271] addressed pr comments

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/forms/ExhibitionConfigurationForm.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/forms/ExhibitionConfigurationForm.java
@@ -30,42 +30,55 @@ public class ExhibitionConfigurationForm {
     public String getCustomMessage() {
         return customMessage;
     }
-    public String getDefaultExhibitLanguage() {
-        return defaultExhibitLanguage;
-    }
-    public String getExhibitionParam() {
-        return exhibitionParam;
-    }
-    public List<String> getExhibitLanguage() {
-        return exhibitLanguage;
-    }
-    public ExhibitionModes getExhibitMode() {
-        return exhibitMode;
-    }
-    public String getSpaceParam() {
-        return spaceParam;
-    }
-    public String getTitle() {
-        return title;
-    }
+
     public void setCustomMessage(String customMessage) {
         this.customMessage = customMessage;
     }
+
+    public String getDefaultExhibitLanguage() {
+        return defaultExhibitLanguage;
+    }
+
     public void setDefaultExhibitLanguage(String defaultExhibitLanguage) {
         this.defaultExhibitLanguage = defaultExhibitLanguage;
     }
+
+    public String getExhibitionParam() {
+        return exhibitionParam;
+    }
+
     public void setExhibitionParam(String exhibitionParam) {
         this.exhibitionParam = exhibitionParam;
     }
+
+    public List<String> getExhibitLanguage() {
+        return exhibitLanguage;
+    }
+
     public void setExhibitLanguage(List<String> exhibitLanguage) {
         this.exhibitLanguage = exhibitLanguage;
     }
+
+    public ExhibitionModes getExhibitMode() {
+        return exhibitMode;
+    }
+
     public void setExhibitMode(ExhibitionModes exhibitMode) {
         this.exhibitMode = exhibitMode;
-    }  
+    }
+
+    public String getSpaceParam() {
+        return spaceParam;
+    }
+
     public void setSpaceParam(String spaceParam) {
         this.spaceParam = spaceParam;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
