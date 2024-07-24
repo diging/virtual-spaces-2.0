6181c3c0e775ddbd20e0ade15798c636529e26ae
ajay3568yadav <89062459+ajay3568yadav@users.noreply.github.com>
2024-07-23 18:13:26 -0700

    VSPC-196 edit external links fixed

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditExternalLinkController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditExternalLinkController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
+import edu.asu.diging.vspace.core.exception.ImageDoesNotExistException;
 import edu.asu.diging.vspace.core.exception.ImageCouldNotBeStoredException;
 import edu.asu.diging.vspace.core.exception.LinkDoesNotExistsException;
 import edu.asu.diging.vspace.core.exception.SpaceDoesNotExistException;
@@ -20,6 +20,22 @@ import edu.asu.diging.vspace.core.model.display.IExternalLinkDisplay;
 import edu.asu.diging.vspace.core.services.IExternalLinkManager;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
 import edu.asu.diging.vspace.core.services.impl.ExternalLinkManager;
+
+
+
+
+
+//@RequestMapping(value = "/staff/space/link/external/{id}", method = RequestMethod.POST)
+//public ResponseEntity<String> createExternalLink(@PathVariable("id") String id, @RequestParam("x") String x,
+//        @RequestParam("y") String y, @RequestParam("externalLinkLabel") String title,
+//        @RequestParam("url") String externalLink,
+//        @RequestParam("externalLinkIdValueEdit") String externalLinkIdValueEdit,
+//        @RequestParam("externalLinkDisplayId") String externalLinkDisplayId,
+//        @RequestParam("tabOpen") String howToOpen, @RequestParam("type") String displayType,
+//        @RequestParam("editExternalLinkInfoImage") MultipartFile file,
+//        @RequestParam(value = "editExternalLinkInfo-imageId", required = false) String imageId) throws SpaceDoesNotExistException, IOException,
+//        LinkDoesNotExistsException, NumberFormatException, ImageCouldNotBeStoredException {
+    
 
 @Controller
 public class EditExternalLinkController extends EditSpaceLinksController {
@@ -31,20 +47,22 @@ public class EditExternalLinkController extends EditSpaceLinksController {
     private IExternalLinkManager externalLinkManager;
 
     @RequestMapping(value = "/staff/space/link/external/{id}", method = RequestMethod.POST)
-    public ResponseEntity<String> createExternalLink(@PathVariable("id") String id, @RequestParam("x") String x,
-            @RequestParam("y") String y, @RequestParam("externalLinkLabel") String title,
-            @RequestParam("url") String externalLink,
-            @RequestParam("externalLinkIdValueEdit") String externalLinkIdValueEdit,
-            @RequestParam("externalLinkDisplayId") String externalLinkDisplayId,
-            @RequestParam("tabOpen") String howToOpen, @RequestParam("type") String displayType,
-            @RequestParam("editExternalLinkInfoImage") MultipartFile file,
-            @RequestParam(value = "editExternalLinkInfo-imageId", required = false) String imageId) throws SpaceDoesNotExistException, IOException,
-            LinkDoesNotExistsException, NumberFormatException, ImageCouldNotBeStoredException {
+	public ResponseEntity<String> createExternalLink(@PathVariable("id") String id, @RequestParam("x") String x,
+			@RequestParam("y") String y, @RequestParam("externalLinkLabel") String title,
+			@RequestParam("url") String externalLink,
+			@RequestParam("externalLinkIdValueEdit") String externalLinkIdValueEdit,
+			@RequestParam("externalLinkDisplayId") String externalLinkDisplayId,
+			@RequestParam("tabOpen") String howToOpen, @RequestParam("type") String displayType,
+			@RequestParam("editExternalLinkInfoImage") MultipartFile file,
+			@RequestParam(value = "editExternalLinkInfo-imageId", required = false) String imageId)
+			throws SpaceDoesNotExistException, IOException, LinkDoesNotExistsException, NumberFormatException,
+			ImageCouldNotBeStoredException, ImageDoesNotExistException {
 
         ResponseEntity<String> validation = checkIfSpaceExists(spaceManager, id, x, y);
         if (validation != null) {
             return validation;
         }
+        String desc = "";
         byte[] linkImage = null;
         String filename = null;
         if (file != null) {
@@ -53,13 +71,13 @@ public class EditExternalLinkController extends EditSpaceLinksController {
         }
         DisplayType type = displayType.isEmpty() ? null : DisplayType.valueOf(displayType);
         ExternalLinkDisplayMode externalLinkOpenMode = howToOpen.isEmpty() ? null
-                : ExternalLinkDisplayMode.valueOf(howToOpen);
+				: ExternalLinkDisplayMode.valueOf(howToOpen);
         IExternalLinkDisplay display = (IExternalLinkDisplay) externalLinkManager.updateLink(title, id, new Float(x),
-                new Float(y), 0, externalLink, title, externalLinkIdValueEdit, externalLinkDisplayId, type, linkImage,
-                filename, externalLinkOpenMode);
+				new Float(y), 0, externalLink, title,desc, externalLinkIdValueEdit, externalLinkDisplayId, type, linkImage,
+				filename, imageId);
         return success(display.getExternalLink().getId(), display.getId(), display.getPositionX(),
-                display.getPositionY(), display.getRotation(), display.getExternalLink().getExternalLink(), title,
-                displayType, null, null);
+				display.getPositionY(), display.getRotation(), display.getExternalLink().getExternalLink(), title,
+				displayType, null, null);
 
     }
 }
