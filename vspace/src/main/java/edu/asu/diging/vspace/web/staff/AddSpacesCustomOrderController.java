9b383fe33002b2b024f6e42499030fc6c0a87d19
pooja-thalur <poojathalur@yahoo.com>
2024-10-01 18:25:59 -0700

    [VSPC-190] adding validation

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/AddSpacesCustomOrderController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/AddSpacesCustomOrderController.java
@@ -1,18 +1,19 @@
 package edu.asu.diging.vspace.web.staff;
 
-import java.util.List;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
 import edu.asu.diging.vspace.core.services.ISpacesCustomOrderManager;
+import edu.asu.diging.vspace.web.staff.forms.SpacesCustomOrderForm;
 
 @Controller
 public class AddSpacesCustomOrderController {
@@ -30,16 +31,14 @@ public class AddSpacesCustomOrderController {
     }
     
     @RequestMapping(value = "/staff/space/order/customorder/add", method = RequestMethod.POST)
-    public String createCustomOrder(Model model, @RequestParam("spaceOrder") List<String> spaceOrder,
-            @RequestParam("name") String name,
-            @RequestParam("description") String description, RedirectAttributes attributes) {
-        if(name == null || name.isEmpty()) {
-            attributes.addFlashAttribute("alertType", "danger");
-            attributes.addFlashAttribute("message", "Custom order could not be saved. Please enter the name of the order.");
-            attributes.addFlashAttribute("showAlert", "true");
-            return "redirect:/staff/space/order/add";
+    public String createCustomOrder(Model model, @Valid @ModelAttribute SpacesCustomOrderForm spacesCustomOrderForm, 
+            BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            model.addAttribute("spacesCustomOrderForm",spacesCustomOrderForm);
+            model.addAttribute("spaces", spaceManager.getAllSpaces());
+            return "staff/spaces/customorder/add";
         }
-        spacesCustomOrderManager.create(spaceOrder, name, description);
+        spacesCustomOrderManager.create(spacesCustomOrderForm.getOrderedSpaces(), spacesCustomOrderForm.getName(), spacesCustomOrderForm.getDescription());
         attributes.addFlashAttribute("alertType", "success");
         attributes.addFlashAttribute("message", "Custom Order has been successfully added.");
         attributes.addFlashAttribute("showAlert", "true");
