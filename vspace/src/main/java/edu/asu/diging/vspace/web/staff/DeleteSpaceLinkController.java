204c91b6680b2fac3e580a506bc166fa07b4f5ee
Julian Ophals <jophals@asu.edu>
2024-10-31 12:08:09 -0700

    [VSPC-242] implemented additional PR change like DeleteLinkController

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/DeleteSpaceLinkController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/DeleteSpaceLinkController.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.asu.diging.vspace.core.services.IExternalLinkManager;
 import edu.asu.diging.vspace.core.services.IModuleLinkManager;
-import edu.asu.diging.vspace.core.services.ISlideExternalLinkManager;
 import edu.asu.diging.vspace.core.services.ISpaceLinkManager;
 
 @Controller
-public class DeleteLinkController {
+public class DeleteSpaceLinkController {
 
     @Autowired
     private IModuleLinkManager moduleLinkManager;
@@ -24,9 +23,6 @@ public class DeleteLinkController {
 
     @Autowired
     private IExternalLinkManager externalLinkManager;
-    
-    @Autowired
-    private ISlideExternalLinkManager slideExternalLinkManager;
 
     @RequestMapping(value = "/staff/space/{id}/modulelink/{linkId}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteModuleLink(@PathVariable("id") String spaceId, @PathVariable("linkId") String linkId) {
@@ -43,12 +39,6 @@ public class DeleteLinkController {
     @RequestMapping(value = "/staff/space/{id}/externallink/{linkId}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteExternalLink(@PathVariable("id") String spaceId, @PathVariable("linkId") String linkId) {
         externalLinkManager.deleteLink(linkId);
-        return new ResponseEntity<String>(HttpStatus.OK);
-    }
-    
-    @RequestMapping(value = "/staff/module/{id}/slide/{slideId}/externallink/{linkId}", method = RequestMethod.DELETE)
-    public ResponseEntity<String> deleteSlideExternalLink(@PathVariable("id") String moduleId, @PathVariable("slideId") String slideId, @PathVariable("linkId") String linkId) {
-        slideExternalLinkManager.deleteLink(linkId);
         return new ResponseEntity<String>(HttpStatus.OK);
     }
 }
