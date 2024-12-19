fbe0a92d99240910122ab790272e093750418501
ajay Yadav <ajayyadav@ajays-MacBook-Air-2.local>
2024-12-20 05:00:18 +0530

    VSPC-147 ref block debug

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditBiblioBlockController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditBiblioBlockController.java
@@ -27,22 +27,31 @@ public class EditBiblioBlockController {
     @RequestMapping(value = "/staff/module/{moduleId}/slide/{id}/bibliography/edit", method = RequestMethod.POST)
     public ResponseEntity<String> editTextBlock(@PathVariable("id") String slideId,
             @PathVariable("moduleId") String moduleId, @RequestBody String biblioBlockData) throws IOException {
-
+    	System.out.println("CP1\n\n\n\n\n");
         ObjectMapper mapper = new ObjectMapper();
         JsonNode rootNode = mapper.readTree(biblioBlockData);
-
+        System.out.println("CP2\n\n\n\n\n");
+        System.out.println(rootNode);
+        System.out.println(biblioBlockData);
+        
         String biblioTitle = rootNode.get("biblioTitle").asText();
         String description = rootNode.get("description").asText();
         String id = rootNode.get("id").asText();
+        System.out.println(id);
+        System.out.println("CP3\n\n");
         
         IBiblioBlock biblioBlock = contentBlockManager.getBiblioBlock(id);
         
         if (biblioBlock == null) {
+        	System.out.println("CP4\n\n");
             return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
         }
+        System.out.println("CP5\n\n\n");
         
         biblioBlock.setBiblioTitle(biblioTitle);
         biblioBlock.setDescription(description);
+        
+        System.out.println(biblioBlock);
         contentBlockManager.updateBiblioBlock((BiblioBlock) biblioBlock);
 
         return new ResponseEntity<String>(HttpStatus.OK);
