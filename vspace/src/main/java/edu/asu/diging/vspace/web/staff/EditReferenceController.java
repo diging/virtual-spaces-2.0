ef83e246900901b2d3108f3c06c1bbeef7985f3a
ajay Yadav <ajayyadav@ajays-MacBook-Air-2.local>
2025-01-04 04:10:03 +0530

    VSPC-147 reference showing up again

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditReferenceController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/EditReferenceController.java
@@ -27,64 +27,61 @@ public class EditReferenceController {
     @RequestMapping(value = "/staff/module/{moduleId}/slide/{id}/bibliography/{biblioId}/reference/edit", method = RequestMethod.POST)
     public ResponseEntity<String> editReference(@PathVariable("id") String slideId,
             @PathVariable("moduleId") String moduleId, @PathVariable("biblioId") String biblioId, 
-            @RequestBody Reference ref) throws IOException {
+            @RequestBody String ref) throws IOException {
     	
-    	System.out.print("XXXXXX\n\n\n\n\n\n\nXXXXXX");
-    	System.out.print(ref.getId());
-    	System.out.print("YYYYXXXXXX\n\n\n\n\n\n\nXXXXXX");
-    	
-    	IReference reference = referenceManager.getReference(ref.getId());
-        reference.setTitle(ref.getTitle());
-        reference.setAuthor(ref.getAuthor());
-        reference.setYear(ref.getYear());
-        reference.setJournal(ref.getJournal());
-        reference.setUrl(ref.getUrl());
-        reference.setVolume(ref.getVolume());
-        reference.setIssue(ref.getIssue());
-        reference.setPages(ref.getPages());
-        reference.setEditors(ref.getEditors());
-        reference.setType(ref.getType());
-        reference.setNote(ref.getNote());
-        referenceManager.updateReference((Reference) reference);
-        
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode rootNode = mapper.readTree(ref);
-//        
-//        System.out.println(ref);
-//        
-//        String title = rootNode.get("title").asText();
-//        String author = rootNode.get("author").asText();
-//        String year = rootNode.get("year").asText();
-//        String journal = rootNode.get("journal").asText();
-//        String url = rootNode.get("url").asText();
-//        String volume = rootNode.get("volume").asText();
-//        String issue = rootNode.get("issue").asText();
-//        String pages = rootNode.get("pages").asText();
-//        String editor = rootNode.get("editors").asText();
-//        String type = rootNode.get("type").asText();
-//        String note = rootNode.get("note").asText();
-//        String id = rootNode.get("id").asText();
-//        
-//        IReference reference = referenceManager.getReference(id);
-//        System.out.print("XXXXXX\n\n\n\n\n\n\nXXXXXX");
-//        System.out.print(id);
-//        if (reference == null) {
-//        	System.out.print("XXXXXXYYY\n\n\n\n\n\n\nXXXXXXYYYY");
-//            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
-//        }
-//        
-//        reference.setTitle(title);
-//        reference.setAuthor(author);
-//        reference.setYear(year);
-//        reference.setJournal(journal);
-//        reference.setUrl(url);
-//        reference.setVolume(volume);
-//        reference.setIssue(issue);
-//        reference.setPages(pages);
-//        reference.setEditors(editor);
-//        reference.setType(type);
-//        reference.setNote(note);
+//    	System.out.print("XXXXXX\n\n\n\n\n\n\nXXXXXX");
+//    	System.out.print(ref.getId());
+//    	System.out.print("YYYYXXXXXX\n\n\n\n\n\n\nXXXXXX");
+//    	
+//    	IReference reference = referenceManager.getReference(ref.getId());
+//        reference.setTitle(ref.getTitle());
+//        reference.setAuthor(ref.getAuthor());
+//        reference.setYear(ref.getYear());
+//        reference.setJournal(ref.getJournal());
+//        reference.setUrl(ref.getUrl());
+//        reference.setVolume(ref.getVolume());
+//        reference.setIssue(ref.getIssue());
+//        reference.setPages(ref.getPages());
+//        reference.setEditors(ref.getEditors());
+//        reference.setType(ref.getType());
+//        reference.setNote(ref.getNote());
 //        referenceManager.updateReference((Reference) reference);
+        
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(ref);
+        
+        System.out.println(ref);
+        
+        String title = rootNode.get("title").asText();
+        String author = rootNode.get("author").asText();
+        String year = rootNode.get("year").asText();
+        String journal = rootNode.get("journal").asText();
+        String url = rootNode.get("url").asText();
+        String volume = rootNode.get("volume").asText();
+        String issue = rootNode.get("issue").asText();
+        String pages = rootNode.get("pages").asText();
+        String editor = rootNode.get("editors").asText();
+        String type = rootNode.get("type").asText();
+        String note = rootNode.get("note").asText();
+        String id = rootNode.get("id").asText();
+        
+        IReference reference = referenceManager.getReference(id);
+        if (reference == null) {
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        }
+        
+        reference.setTitle(title);
+        reference.setAuthor(author);
+        reference.setYear(year);
+        reference.setJournal(journal);
+        reference.setUrl(url);
+        reference.setVolume(volume);
+        reference.setIssue(issue);
+        reference.setPages(pages);
+        reference.setEditors(editor);
+        reference.setType(type);
+        reference.setNote(note);
+        referenceManager.updateReference((Reference) reference);
 
         return new ResponseEntity<String>(HttpStatus.OK);
     }
