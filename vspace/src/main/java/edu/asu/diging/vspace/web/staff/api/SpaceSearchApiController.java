46524a28a3d5ea6f5c220a17466808213c43a19f
ajay Yadav <ajayyadav@Mac.attlocal.net>
2025-01-24 16:55:39 -0700

    VSPC-217 created spacesearch controller api

---
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/SpaceSearchApiController.java
@@ -0,0 +1,46 @@
+package edu.asu.diging.vspace.web.staff.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import edu.asu.diging.vspace.core.model.IVSImage;
+import edu.asu.diging.vspace.core.services.IImageService;
+import edu.asu.diging.vspace.core.services.ISpaceManager;
+
+public class SpaceSearchApiController {
+    @Autowired
+    private ISpaceManager spaceManager;
+
+    @RequestMapping("/staff/spaces/search/")
+    public ResponseEntity<String> searchSpace(@RequestParam(value = "term", required = false) String searchTerm) {
+        List<IVSImage> images = null;
+        if (searchTerm != null && !searchTerm.trim().isEmpty()) {
+            images = spaceManager.getSpace(searchTerm);
+        } else {
+            images = imageService.getImages(1);
+        }
+        
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode idArray = mapper.createArrayNode();
+        for (ISpace image : images) {
+            ObjectNode imageNode = mapper.createObjectNode();
+            imageNode.put("id", image.getId());
+            if (image.getName() != null && !image.getName().isEmpty()) {
+                imageNode.put("text", image.getName() + " (" + image.getFilename() + ")");
+            } else {
+                 imageNode.put("text", image.getFilename());
+            }
+            idArray.add(imageNode);
+        }
+        return new ResponseEntity<String>(idArray.toString(), HttpStatus.OK);
+    }
+}
