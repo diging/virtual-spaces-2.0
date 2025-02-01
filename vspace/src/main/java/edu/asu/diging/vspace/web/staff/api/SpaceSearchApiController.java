720f8beea5e0f849fa44a64badbcfdd13ff3752c
ajay Yadav <ajayyadav@Mac.attlocal.net>
2025-01-31 17:03:27 -0700

    VSPC-217 review comments

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/SpaceSearchApiController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/SpaceSearchApiController.java
@@ -1,14 +1,11 @@
 package edu.asu.diging.vspace.web.staff.api;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -16,32 +13,32 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import edu.asu.diging.vspace.core.exception.SpaceDoesNotExistException;
 import edu.asu.diging.vspace.core.model.ISpace;
-import edu.asu.diging.vspace.core.model.IVSImage;
-import edu.asu.diging.vspace.core.services.IImageService;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@RestController
+@RequestMapping("/staff/search/api/space")
 public class SpaceSearchApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpaceSearchApiController.class);
+
     @Autowired
     private ISpaceManager spaceManager;
 
-    @RequestMapping("/staff/search/api/space/{id}")
-    public ResponseEntity<String> searchSpace(@PathVariable("id") String id)
-    		throws NumberFormatException, SpaceDoesNotExistException, IOException {
-    	System.out.println("\n\n\n\n\n\n stage11 \n\n\n\n\n");
-    	System.out.println(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<String> searchSpace(@PathVariable("id") String id) {
+        logger.info("Received search request for space ID: {}", id);
+
         ISpace space = spaceManager.getSpace(id);
-        
+
         if (space == null) {
-            return new ResponseEntity<>("{'error': 'Space could not be found.'}", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("{\"error\": \"Space could not be found.\"}", HttpStatus.NOT_FOUND);
         }
-        System.out.println("stage1");
-        ObjectMapper mapper = new ObjectMapper();
-        ArrayNode itemsArray = mapper.createArrayNode();
-        ObjectNode spaceNode = mapper.createObjectNode();
-        spaceNode.put("id", space.getId());
-        spaceNode.put("name", space.getName());
-        itemsArray.add(spaceNode);
-        
-        return new ResponseEntity<String>(itemsArray.toString(), HttpStatus.OK);
+
+        logger.info("Space found: {}", space.getName());
+
+        return new ResponseEntity<>("\"" + space.getName() + "\"", HttpStatus.OK);
     }
+
 }
