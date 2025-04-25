b861af116d55cd59cd28352d0bc3043a0edffa59
ajay3568yadav <ajay3568@gmail.com>
2025-04-25 15:04:34 -0700

    VSPC review comments json

--- vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/SpaceSearchApiController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/staff/api/SpaceSearchApiController.java
@@ -1,41 +1,38 @@
 package edu.asu.diging.vspace.web.staff.api;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import edu.asu.diging.vspace.core.exception.SpaceDoesNotExistException;
 import edu.asu.diging.vspace.core.model.ISpace;
 import edu.asu.diging.vspace.core.services.ISpaceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/staff/search/api/space")
 public class SpaceSearchApiController {
-
     private static final Logger logger = LoggerFactory.getLogger(SpaceSearchApiController.class);
-
+    
     @Autowired
     private ISpaceManager spaceManager;
-
+    
     @GetMapping("/{id}")
-    public ResponseEntity<String> searchSpace(@PathVariable("id") String id) {
-
+    public ResponseEntity<?> searchSpace(@PathVariable("id") String id) {
         ISpace space = spaceManager.getSpace(id);
-
+        
         if (space == null) {
-            return new ResponseEntity<>("{\"error\": \"Space could not be found.\"}", HttpStatus.NOT_FOUND);
+            Map<String, String> response = new HashMap<>();
+            response.put("error", "Space could not be found.");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-
-        return new ResponseEntity<>("\"" + space.getName() + "\"", HttpStatus.OK);
+        
+        Map<String, String> response = new HashMap<>();
+        response.put("name", space.getName());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
 }
