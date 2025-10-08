f3b8ec2278229d8ccb5266c986c7e87393768b6d
Girik1105 <girik1105@gmail.com>
2025-10-08 12:08:46 -0700

    [VSPC-216] Added language navigation in about and slides

--- vspace/src/main/java/edu/asu/diging/vspace/web/HomeController.java
+++ vspace/src/main/java/edu/asu/diging/vspace/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.asu.diging.simpleusers.core.model.Role;
 import edu.asu.diging.vspace.core.model.IExhibition;
+import edu.asu.diging.vspace.core.model.IExhibitionLanguage;
 import edu.asu.diging.vspace.core.services.IExhibitionManager;
 import edu.asu.diging.vspace.core.services.ISetupManager;
 import edu.asu.diging.vspace.web.exhibit.view.ExhibitionConstants;
@@ -36,13 +37,17 @@ public class HomeController {
         IExhibition exhibition = exhibitionManager.getStartExhibition();
         if (exhibition != null && exhibition.getStartSpace() != null) {
             if (previewId != null) {
-                return "redirect:/preview/{previewId}/space/" + exhibition.getStartSpace().getId();
+                IExhibitionLanguage defaultLanguage = exhibitionManager.getDefaultLanguage(exhibition);
+                String languageCode = defaultLanguage != null ? defaultLanguage.getCode() : "en";
+                return "redirect:/preview/{previewId}/space/" + exhibition.getStartSpace().getId() + "/" + languageCode;
             } else {
                 if (!(authentication instanceof AnonymousAuthenticationToken) &&
                         (authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals(Role.ADMIN) || r.getAuthority().equals("ROLE_STAFF")))) {
                     return "redirect:/staff/dashboard/";
                 } else {
-                    return "redirect:/exhibit/space/" + exhibition.getStartSpace().getId();
+                    IExhibitionLanguage defaultLanguage = exhibitionManager.getDefaultLanguage(exhibition);
+                    String languageCode = defaultLanguage != null ? defaultLanguage.getCode() : "en";
+                    return "redirect:/exhibit/space/" + exhibition.getStartSpace().getId() + "/" + languageCode;
                 }
             }
         }
