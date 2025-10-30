0b966e2d68035149cc70f250c27dea1788876d15
Girik1105 <girik1105@gmail.com>
2025-10-30 11:22:45 -0700

    [VSPC-150] Implementation of references section in staff dashboard

--- vspace/src/main/java/edu/asu/diging/vspace/core/services/IReferenceManager.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/IReferenceManager.java
@@ -2,6 +2,8 @@ package edu.asu.diging.vspace.core.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import edu.asu.diging.vspace.core.model.IBiblioBlock;
 import edu.asu.diging.vspace.core.model.IReference;
 import edu.asu.diging.vspace.core.model.impl.Reference;
@@ -19,5 +21,11 @@ public interface IReferenceManager {
     void deleteReferences(List<IReference> references, String BiblioId);
 
     List<IReference> getReferencesForBiblio(String biblioId);
+    
+    List<IReference> getAllReferences(int pageNo, String sortedBy, String order);
+    
+    long getTotalReferenceCount();
+    
+    long getTotalPages();
 
 }
--- vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/ReferenceManager.java
+++ vspace/src/main/java/edu/asu/diging/vspace/core/services/impl/ReferenceManager.java
@@ -7,11 +7,17 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import edu.asu.diging.vspace.core.data.ReferenceRepository;
 import edu.asu.diging.vspace.core.model.IBiblioBlock;
 import edu.asu.diging.vspace.core.model.IReference;
+import edu.asu.diging.vspace.core.model.SortByField;
 import edu.asu.diging.vspace.core.model.impl.BiblioBlock;
 import edu.asu.diging.vspace.core.model.impl.Reference;
 import edu.asu.diging.vspace.core.services.IContentBlockManager;
@@ -27,6 +33,9 @@ public class ReferenceManager implements IReferenceManager {
     
     @Autowired
     private IContentBlockManager contentBlockManager;
+    
+    @Value("${page_size}")
+    private int pageSize;
 
     @Override
     public IReference createReference(String biblioId, String title, String author,String year,String journal, String url, String volume,String issue, String pages,String editor, String type, String note, String visibility) {
@@ -88,5 +97,48 @@ public class ReferenceManager implements IReferenceManager {
     @Override
     public List<IReference> getReferencesForBiblio(String biblioId) {
         return new ArrayList<>(referenceRepo.findByBiblios_Id(biblioId));
+    }
+    
+    @Override
+    public List<IReference> getAllReferences(int pageNo, String sortedBy, String order) {
+        Sort sortingParameters = getSortingParameters(sortedBy, order);
+        if (pageNo < 1) {
+            pageNo = 1;
+        }
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sortingParameters);
+        Page<Reference> references = referenceRepo.findAll(pageable);
+        if (references.getContent().size() == 0 && references.getTotalPages() > 0) {
+            pageable = PageRequest.of(references.getTotalPages() - 1, pageSize, sortingParameters);
+            references = referenceRepo.findAll(pageable);
+        }
+        List<IReference> results = new ArrayList<>();
+        if (references != null) {
+            references.getContent().forEach(ref -> results.add(ref));
+        }
+        return results;
+    }
+    
+    @Override
+    public long getTotalReferenceCount() {
+        return referenceRepo.count();
+    }
+    
+    @Override
+    public long getTotalPages() {
+        long count = referenceRepo.count();
+        return (count % pageSize == 0) ? count / pageSize : (count / pageSize) + 1;
+    }
+    
+    private Sort getSortingParameters(String sortedBy, String order) {
+        Sort sortingParameters = Sort.by(SortByField.CREATION_DATE.getValue()).descending();
+        if (sortedBy != null && SortByField.getAllValues().contains(sortedBy)) {
+            sortingParameters = Sort.by(sortedBy);
+        }
+        if (order != null && order.equalsIgnoreCase(Sort.Direction.ASC.toString())) {
+            sortingParameters = sortingParameters.ascending();
+        } else {
+            sortingParameters = sortingParameters.descending();
+        }
+        return sortingParameters;
     }
 }
